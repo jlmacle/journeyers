7c3a3651425daee927f33d18778ccb7129fbaed5
unknown <jeanlouis.macle@gmail.com>
2025-11-12 03:32:09 -0600

    Test added for file utils

---
+++ test/core/utils/file_utils_test.dart
@@ -0,0 +1,33 @@
+import "dart:io";
+
+import 'package:test/test.dart';
+
+import 'package:journeyers/core/utils/file_utils.dart';
+
+void main()
+{
+  var fileUtils = FileUtils();
+  var tempDir = Directory.current.createTempSync('file_utils_test');
+
+  group('File Utils tests:', () 
+  {
+    test('the content to write should be found in the file', () async
+    {
+      // using the library
+      var testFilePath = '${tempDir.path}${Platform.pathSeparator}test_file.txt';
+      var contentToWrite = 'Hello world';
+      fileUtils.appendText(testFilePath, contentToWrite);
+
+      // testing if the content was written
+      var testFile = File(testFilePath);
+      var content = await testFile.readAsString();
+      expect(content, equals(contentToWrite));
+
+      // cleaning the temporary files
+      if (tempDir.existsSync())
+      {
+        tempDir.delete(recursive: true);
+    }
+    });
+  });
+}
