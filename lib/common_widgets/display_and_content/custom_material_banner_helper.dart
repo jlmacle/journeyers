f90fb5c19aae451c41a36f835ed7271ee72c2724
unknown <jeanlouis.macle@gmail.com>
2025-11-15 13:16:37 -0600

    External code for material banner added

---
+++ lib/common_widgets/display_and_content/custom_material_banner_helper.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void showCustomMaterialBanner({required BuildContext buildContext, required String message, required Color messageColor,
+required IconData iconData, required Color iconColor, 
+required String actiontext, required Color actionTextColor, required FontWeight actionTextFontweight}) {
+  final scaffoldMessenger = ScaffoldMessenger.of(buildContext);
+
+  void hideBanner() {
+    scaffoldMessenger.hideCurrentMaterialBanner();
+  }
+
+  scaffoldMessenger.showMaterialBanner(
+    MaterialBanner(
+      content: Text(
+        message,
+        style: TextStyle(color: messageColor),
+      ),
+      leading: Icon(iconData, color: iconColor),
+      actions: <Widget>[
+        TextButton(
+          onPressed: hideBanner, 
+          child: Text(
+            actiontext,
+            style: TextStyle(color: actionTextColor, fontWeight: actionTextFontweight),
+          ),
+        ),
+      ],
+    ),
+  );
+}
