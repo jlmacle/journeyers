f34f9f0122f2e699059150a74291ea413d9e8455
unknown <jeanlouis.macle@gmail.com>
2025-11-17 13:45:30 -0600

    JSON utils code added

---
+++ lib/core/utils/json_utils.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:logging/logging.dart';
+
+final logger = Logger("Json Utils");
+
+
+
+///
+/// This method assumes for the json file to be simple hashmap
+///
+Future<void> savePreference({required String jsonFilePath,required String preferenceName,required dynamic preferenceValue}) async
+{
+  var jsonFile = File(jsonFilePath);
+  Map<String,dynamic> newData = {preferenceName:preferenceValue};
+  String jsonString = jsonEncode(newData);
+
+  //If the file doesn't exist
+  if (! jsonFile.existsSync())
+  {
+    // File creation with the new data
+    try{await jsonFile.writeAsString(jsonString);}
+    on Exception catch (e){logger.shout("Error while writing the Json String");logger.shout(e);} 
+  }
+  else
+  {
+    try
+    {
+      jsonString = await jsonFile.readAsString();
+      Map<String,dynamic>  map = jsonDecode(jsonString);
+      map.addEntries(newData.entries);
+      String updatedJsonString = jsonEncode(map);
+      await jsonFile.writeAsString(updatedJsonString);
+    }
+    on Exception catch (e){logger.shout("Error while readding/decoding the Json String");logger.shout(e);} 
+  }
+}
