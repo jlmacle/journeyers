17997bd896f3555a8b3536077dbd4ae06ec8e316
unknown <jeanlouis.macle@gmail.com>
2025-11-17 10:14:41 -0600

    Code cleanup

--- test/core/utils/file_utils_test.dart
+++ test/core/utils/file_utils_test.dart
@@ -1,5 +1,6 @@
 import "dart:io";
 
+import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
 import 'package:journeyers/core/utils/file_utils.dart';
@@ -7,28 +8,29 @@ import 'package:journeyers/core/utils/file_utils.dart';
 void main()
 {
   var fileUtils = FileUtils();
-  var tempDir = Directory.current.createTempSync('file_utils_test');
+  var testFilePath = path.join("test","core","utils","file_utils_test_data","file.txt");
 
   group('File utils tests:', () 
   {
     test('the content to write should be found in the file', () async
     {
-      // using the library
-      var testFilePath = '${tempDir.path}${Platform.pathSeparator}test_file.txt';
+      
       var contentToWrite = 'Hello world';
       var file = File(testFilePath);
+
+      // deleting the file if existant
+      if (file.existsSync())
+      {
+        await file.delete();
+      }
+      // using the library
       fileUtils.appendText(file, contentToWrite);
 
       // testing if the content was written
       var testFile = File(testFilePath);
       var content = await testFile.readAsString();
-      expect(content, equals(contentToWrite));
-
-      // cleaning the temporary files
-      if (tempDir.existsSync())
-      {
-        tempDir.delete(recursive: true);
-    }
+      expect(content, equals(contentToWrite));      
+      
     });
   });
 }
