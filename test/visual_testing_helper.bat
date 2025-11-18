b0b4048af6f906b3250658ae8e77b37b547ae7c8
unknown <jeanlouis.macle@gmail.com>
2025-11-18 04:40:07 -0600

    Helper for the widgets visual testing (intermediate version)

---
+++ test/visual_testing_helper.bat
@@ -0,0 +1,16 @@
+@echo off
+set BROWSER="C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+echo Programm to wait for the web servers to start before opening browser tabs
+timeout /t 7 >nul
+start flutter run -t .\test\common_widgets\display_and_content\custom_dismissable_rectangular_area_visual_testing.dart -d web-server --web-port 8080  &
+start flutter run -t .\test\common_widgets\display_and_content\custom_material_banner_visual_testing.dart -d web-server --web-port 8081  &
+ 
+timeout /t 20 >nul
+
+%BROWSER% "http://localhost:8080"
+%BROWSER% "http://localhost:8081"
+
+
+
+
