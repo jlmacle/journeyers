4d09dd31b02ed931bc224e4545e1c1905d4eec76
unknown <jeanlouis.macle@gmail.com>
2025-11-18 10:55:12 -0600

    Code for semi-automation of  widgets visual testing

--- test/visual_testing_helper.bat
+++ test/visual_testing_helper.bat
@@ -1,16 +1,16 @@
+cd ..
 @echo off
 set BROWSER="C:\Program Files\Google\Chrome\Application\chrome.exe"
-
 echo Programm to wait for the web servers to start before opening browser tabs
-timeout /t 7 >nul
-start flutter run -t .\test\common_widgets\display_and_content\custom_dismissable_rectangular_area_visual_testing.dart -d web-server --web-port 8080  &
-start flutter run -t .\test\common_widgets\display_and_content\custom_material_banner_visual_testing.dart -d web-server --web-port 8081  &
- 
-timeout /t 20 >nul
-
-%BROWSER% "http://localhost:8080"
-%BROWSER% "http://localhost:8081"
-
-
-
-
+timeout /t 5 >nul
+start flutter run -t .\test\common_widgets\display_and_content\custom_dismissable_rectangular_area_visual_testing.dart -d web-server --web-port 8091
+start flutter run -t .\test\common_widgets\display_and_content\custom_material_banner_visual_testing.dart -d web-server --web-port 8092
+start flutter run -t .\test\common_widgets\display_and_content\custom_snackbar_start_message_visual_testing.dart -d web-server --web-port 8093
+start flutter run -t .\test\common_widgets\interaction_and_inputs\custom_language_switcher_visual_testing.dart -d web-server --web-port 8094
+start flutter run -t .\test\common_widgets\lists_and_scrolling\custom_expansion_tile_visual_testing.dart -d web-server --web-port 8095
+timeout /t 30 >nul
+%BROWSER% "http://localhost:8091
+%BROWSER% "http://localhost:8092
+%BROWSER% "http://localhost:8093
+%BROWSER% "http://localhost:8094
+%BROWSER% "http://localhost:8095
