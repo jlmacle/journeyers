1a0cd11482bbc179d3a3e7e7e9da65bdc699f861
unknown <jeanlouis.macle@gmail.com>
2025-11-18 09:36:30 -0600

    Code cleanup

--- lib/core/utils/files_and_json/file_utils.dart
+++ lib/core/utils/files_and_json/file_utils.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:logging/logging.dart';
 
 import 'package:journeyers/core/utils/printing_and_logging/logging_utils.dart';
+import 'package:path/path.dart' as path;
 
 class FileUtils 
 {
@@ -13,9 +14,13 @@ class FileUtils
  
   final logger = Logger("file_utils.dart");
 
-  Future<void> appendText(File file, String text) async
+  ///
+  ///
+  ///
+  Future<void> appendText({required String filePath, required String text}) async
   {
     try{
+      File file = File(filePath);
       var sink = file.openWrite(mode: FileMode.append);
       sink.write(text);
       await sink.flush();
@@ -23,6 +28,42 @@ class FileUtils
     }
     on FileSystemException catch (e) {logger.shout('Error appending text to file: ${e.message}'); }
     catch(e) {logger.shout('Error appending text to file: $e');}
+  }
+  
+  ///
+  ///
+  ///
+  Future<void> createFileAndAddContent({required String filePath, required String text}) async
+  {
+    try{
+      File file = File(filePath);
+      var sink = file.openWrite(mode: FileMode.write);
+      sink.write(text);
+      await sink.flush();
+      await sink.close();
+    }
+    on FileSystemException catch (e) {logger.shout('Error writitng text to file: ${e.message}'); }
+    catch(e) {logger.shout('Error writitng text to file: $e');}
+  }
+
+  ///
+  /// This method assumes a file extension with the format .file_extension
+  ///
+  List<File> getFilesInDirectory({required String directoryPath, required String fileExtension, required bool searchIsRecursive, bool followsLinks = false})
+  {
+    List<File> fileList = [];
+    Directory fileDir = Directory(directoryPath);
+    List<FileSystemEntity> entityList  = fileDir.listSync(recursive: searchIsRecursive, followLinks: followsLinks);
+    for (var entity in entityList)
+    {
+      if (entity is File)
+      {
+        String fileName = path.basename(entity.path);
+        if (fileName.endsWith(fileExtension)) fileList.add(entity);
+      }
+    }
+
+    return fileList;
   }
 
 }
