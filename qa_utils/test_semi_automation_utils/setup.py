b0734dccfbd278ef368e3f8e1bcb42c6448cc987
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-11-22 17:04:57 +0100

    Python utils added

---
+++ qa_utils/test_semi_automation_utils/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py_utils",
+    version="0.1",
+    packages=["py_utils"],  # or find_packages() now that layout is standard
+)
+
+# pip install -e .  in the setup.py directory
+# pip show py_utils
