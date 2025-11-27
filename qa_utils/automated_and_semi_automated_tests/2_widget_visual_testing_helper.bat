cab2f8eaf0a8c87b1afc0f03afcd128a85be1104
unknown <jeanlouis.macle@gmail.com>
2025-11-27 08:39:37 -0600

    Windows script updated

--- qa_utils/automated_and_semi_automated_tests/2_widget_visual_testing_helper.bat
+++ qa_utils/automated_and_semi_automated_tests/2_widget_visual_testing_helper.bat
@@ -8,9 +8,10 @@ timeout /t 5 >nul
 
 start  flutter run -t ./test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart  -d web-server --web-port  8091
 start  flutter run -t ./test/common_widgets/display_and_content/custom_snackbar_start_message_helper_visual_testing.dart  -d web-server --web-port  8092
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switcher_visual_testing.dart  -d web-server --web-port  8093
-start  flutter run -t test/common_widgets/interaction_and_inputs/custom_segmented_button_with_optional_text_field_visual_testing.dart  -d web-server --web-port  8094
-start  flutter run -t ./test/common_widgets/lists_and_scrolling/custom_expansion_tile_visual_testing.dart  -d web-server --web-port  8095
+start  flutter run -t test/common_widgets/interaction_and_inputs/custom_cascading_checkbox_visual_testing.dart  -d web-server --web-port  8093
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switcher_visual_testing.dart  -d web-server --web-port  8094
+start  flutter run -t test/common_widgets/interaction_and_inputs/custom_segmented_button_with_optional_text_field_visual_testing.dart  -d web-server --web-port  8095
+start  flutter run -t ./test/common_widgets/lists_and_scrolling/custom_expansion_tile_visual_testing.dart  -d web-server --web-port  8096
 
 :: Waiting for the web servers to start
 timeout /t 70 >nul 
@@ -19,3 +20,4 @@ timeout /t 70 >nul
 %BROWSER% http://localhost:8093
 %BROWSER% http://localhost:8094
 %BROWSER% http://localhost:8095
+%BROWSER% http://localhost:8096
