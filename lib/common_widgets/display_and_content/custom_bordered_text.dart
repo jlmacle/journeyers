153f1ab11d545b8c2830e74794669678658e8999
unknown <jeanlouis.macle@gmail.com>
2025-11-29 13:00:28 -0600

    Code cleanup

--- lib/common_widgets/display_and_content/custom_bordered_text.dart
+++ lib/common_widgets/display_and_content/custom_bordered_text.dart
@@ -3,17 +3,17 @@ import 'package:journeyers/common_widgets/display_and_content/custom_text.dart';
 
 class CustomBorderedText extends StatelessWidget 
 {
-  /// The CustomText object containing the text
+  /// The CustomText containing the text
   final CustomText customText;
-  /// The padding information encapsulated in an EdgeInsetsGeometry object
+  /// The padding information encapsulated in an EdgeInsetsGeometry
   final EdgeInsetsGeometry edgeInsetsGeometry;
-  /// The Border object used to pass the border information
+  /// The Border used to pass the border information
   final Border border;
   /// The color of the border
   final Color borderColor;
   /// The width of the border
   final double borderWidth;
-  /// The BorderRadius object used to pass the border radius information
+  /// The BorderRadius used to pass the border radius information
   final BorderRadius borderRadius;
 
   const CustomBorderedText
