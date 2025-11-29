c2c0a98c5dbaadbbc403bea14ce5d4eff0e2ef4f
unknown <jeanlouis.macle@gmail.com>
2025-11-29 11:30:09 -0600

    Test added for custom bordered text

---
+++ test/common_widgets/display_and_content/custom_bordered_text_visual_testing.dart
@@ -0,0 +1,46 @@
+//Line for automated processing
+// flutter run -t test/common_widgets/display_and_content/custom_bordered_text_visual_testing.dart -d linux
+// flutter run -t test/common_widgets/display_and_content/custom_bordered_text_visual_testing.dart -d macos
+// flutter run -t test/common_widgets/display_and_content/custom_bordered_text_visual_testing.dart -d windows
+//Line for automated processing
+import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart'; // https://api.flutter.dev/flutter/rendering/
+
+import 'package:journeyers/app_themes.dart';
+import 'package:journeyers/common_widgets/display_and_content/custom_bordered_text.dart';
+import 'package:journeyers/common_widgets/display_and_content/custom_text.dart';
+
+void main() 
+{  
+  WidgetsFlutterBinding.ensureInitialized(); // was not necessary on Windows, was necessary for macOS
+  // debugPaintSizeEnabled = true;
+  runApp(const MyTestingApp());
+}
+
+class MyTestingApp extends StatelessWidget 
+{
+  const MyTestingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
+      theme: appTheme, 
+      home: Scaffold
+      (
+        appBar: AppBar
+        (
+          title: const Text('MyTestingApp'),
+        ),
+        body: Center
+        (
+          child:  
+          CustomBorderedText(
+            customTextObject: CustomText(text: "Custom bordered text", color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold, textAlign: TextAlign.center),
+            edgeInsetsGeometryObject: EdgeInsets.all(20), borderObject: Border.all(), borderRadiusObject: BorderRadius.circular(10))
+        )
+      )
+    );
+   }
+}
