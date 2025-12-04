20f5e5926794f759ad34b2d4a396ab69a6f55be8
unknown <jeanlouis.macle@gmail.com>
2025-12-04 06:49:57 -0600

    Notes related to pytest added to setup.py

--- qa_utils/utils_semi_automated_testing/setup.py
+++ qa_utils/utils_semi_automated_testing/setup.py
@@ -8,3 +8,6 @@ setup(
 
 # pip install -e .  (in the directory parent to setup.py) 
 # pip show py_utils
+# If 'pip show py_utils' returns a location similar to 'Python312\Lib\site-packages', 
+# 'python3.12 -m pytest' might be necessary to run pytest without import issues.
+
