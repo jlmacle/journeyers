141106cc381a7ea49ac0fd2e963f933d60fa96d7
unknown <jeanlouis.macle@gmail.com>
2025-12-05 13:43:52 -0600

    Test for the custom checkbox list tile added

---
+++ test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart
@@ -0,0 +1,106 @@
+//Line for automated processing
+// flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart -d chrome
+// flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart -d linux
+// flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart -d macos
+// flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart -d windows
+//Line for automated processing
+
+
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:journeyers/app_themes.dart';
+import 'package:journeyers/common_widgets/display_and_content/custom_text.dart';
+import 'package:journeyers/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field.dart';
+
+void main() 
+{  
+  // WidgetsFlutterBinding.ensureInitialized(); // was not necessary on Windows, was necessary for macos
+  runApp(const MyTestingApp());
+}
+
+
+class MyTestingApp extends StatefulWidget 
+{
+  const MyTestingApp({super.key});
+  @override
+  State<MyTestingApp> createState() => _MyTestingAppState();
+}
+
+
+class _MyTestingAppState extends State<MyTestingApp> 
+{
+  @override
+  Widget build(BuildContext context) 
+  {
+    return MaterialApp
+    (
+      theme: appTheme, 
+      home: HomePage()
+      );
+  }
+}
+//---------------------------------------------------
+
+class HomePage extends StatefulWidget 
+{
+
+  const HomePage
+  ({
+    super.key,
+  });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+{
+  bool? _isCheckboxChecked;
+
+  _setCheckboxState(bool value)
+  {
+    setState(() => _isCheckboxChecked = value);
+  }
+
+  @override
+  void initState() {
+    super.initState();    
+    _isCheckboxChecked = false; 
+  }
+
+  @override
+  Widget build(BuildContext context) 
+  {
+    FocusNode appBarTitleFocusNode = FocusNode();    
+
+    return Scaffold
+    (
+       appBar: AppBar
+      (
+        title: Semantics
+        (
+          focused: true,
+          focusable: true, 
+          child: Focus
+          (
+            focusNode: appBarTitleFocusNode,
+            child: const Text('MyTestingApp'),
+          )
+        ),
+      ),
+      body: Column
+      (
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: 
+        [            
+          CustomCheckBoxWithTextField(text: "Checkbox text", onChanged: _setCheckboxState),
+          Gap(8),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: CustomText(text:"Is the checkbox checked? $_isCheckboxChecked", textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+          )
+        ]
+      ),
+    );
+  }
+}
