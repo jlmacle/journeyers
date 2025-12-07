710f6057f171810538fdbbd2ac05f50c83bf55cf
unknown <jeanlouis.macle@gmail.com>
2025-12-07 07:59:08 -0600

    Additions to appTheme

--- lib/app_themes.dart
+++ lib/app_themes.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 
 final Color navyBlue = Color(0xFF0a2e50);
 final Color paleCyan = Color(0xFFE9FAFC);
+final double postAnalysisHeaderLevel2Gap = 20;
+final double preAndPostDividerGap = 15;
+final double betweenSubPointsDividerHeight = 25;
+
+final TextStyle feedbackMessageStyle = TextStyle(fontSize: 18, fontWeight: FontWeight.normal);
 
 
 final ThemeData appTheme = ThemeData
@@ -32,23 +37,23 @@ final ThemeData appTheme = ThemeData
     textTheme: const TextTheme
     ( // doesn't apply to the appbar
       // https://api.flutter.dev/flutter/material/TextTheme/displayLarge.html
-      displayLarge: TextStyle(color: Colors.black),
+      // displayLarge: TextStyle(color: Colors.black),
       // https://api.flutter.dev/flutter/material/TextTheme/displayMedium.html
-      displayMedium: TextStyle(color: Colors.black),
+      // displayMedium: TextStyle(color: Colors.black),
       // https://api.flutter.dev/flutter/material/TextTheme/displaySmall.html
-      displaySmall: TextStyle(color: Colors.black),
+      // displaySmall: TextStyle(color: Colors.black),
       // https://api.flutter.dev/flutter/material/TextTheme/headlineLarge.html
-      headlineLarge: TextStyle(color: Colors.black, fontSize: 32),
+      headlineLarge: TextStyle(color: Colors.black, fontSize: 24, fontWeight: FontWeight.w900),
       // https://api.flutter.dev/flutter/material/TextTheme/headlineMedium.html
-      headlineMedium: TextStyle(color: Colors.black, fontSize: 28),
+      headlineMedium: TextStyle(color: Colors.black, fontSize: 23, fontWeight: FontWeight.bold,),
       // https://api.flutter.dev/flutter/material/TextTheme/headlineSmall.html
-      headlineSmall: TextStyle(color: Colors.black, fontSize: 24),
+      headlineSmall: TextStyle(color: Colors.black, fontSize: 22),
       // https://api.flutter.dev/flutter/material/TextTheme/titleLarge.html
-      titleLarge: TextStyle(color: Colors.black, fontSize: 22),
+      titleLarge: TextStyle(color: Colors.black, fontSize: 20),
       // https://api.flutter.dev/flutter/material/TextTheme/titleMedium.html
-      titleMedium: TextStyle(color: Colors.black, fontSize: 16),
+      titleMedium: TextStyle(color: Colors.black, fontSize: 18),
       // https://api.flutter.dev/flutter/material/TextTheme/titleSmall.html
-      titleSmall: TextStyle(color: Colors.black, fontSize: 14),
+      titleSmall: TextStyle(color: Colors.black, fontSize: 16),
       bodyLarge: TextStyle(color: Colors.black),
       bodyMedium: TextStyle(color: Colors.black),
       bodySmall: TextStyle(color: Colors.black),
