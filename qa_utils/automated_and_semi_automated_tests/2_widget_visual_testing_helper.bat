5450f338c09770f71f6c4c49efb5266fbdc8605f
unknown <jeanlouis.macle@gmail.com>
2025-12-07 10:18:03 -0600

    Code cleanup

--- qa_utils/automated_and_semi_automated_tests/2_widget_visual_testing_helper.bat
+++ qa_utils/automated_and_semi_automated_tests/2_widget_visual_testing_helper.bat
@@ -6,12 +6,12 @@ cd ../..
 echo "After launching the terminals, programm to wait for the web servers to be completely started before opening the browser tabs"
 timeout /t 5 >nul
 
-start  flutter run -t ./test/common_widgets/display_and_content/custom_bordered_text_visual_testing.dart  -d web-server --web-port  8091
-start  flutter run -t ./test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart  -d web-server --web-port  8092
-start  flutter run -t ./test/common_widgets/display_and_content/custom_header_visual_testing.dart  -d web-server --web-port  8093
-start  flutter run -t ./test/common_widgets/display_and_content/custom_snackbar_start_message_helper_visual_testing.dart  -d web-server --web-port  8094
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart  -d web-server --web-port  8095
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switcher_visual_testing.dart  -d web-server --web-port  8096
+start  flutter run -t ./test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart  -d web-server --web-port  8091
+start  flutter run -t ./test/common_widgets/display_and_content/custom_header_visual_testing.dart  -d web-server --web-port  8092
+start  flutter run -t ./test/common_widgets/display_and_content/custom_snackbar_start_message_helper_visual_testing.dart  -d web-server --web-port  8093
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart  -d web-server --web-port  8094
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switcher_visual_testing.dart  -d web-server --web-port  8095
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_padded_text_field_visual_testing.dart  -d web-server --web-port  8096
 start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart  -d web-server --web-port  8097
 start  flutter run -t ./test/common_widgets/lists_and_scrolling/custom_expansion_tile_visual_testing.dart  -d web-server --web-port  8098
 
