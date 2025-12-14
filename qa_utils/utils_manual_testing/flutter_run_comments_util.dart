6355bdfa45d2db798d225a75eba98efd41b85756
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2025-12-14 18:12:56 +0100

    Code cleanup

--- qa_utils/utils_manual_testing/flutter_run_comments_util.dart
+++ qa_utils/utils_manual_testing/flutter_run_comments_util.dart
@@ -1,15 +1,15 @@
 // Uses Platform, therefore cannot function on a web app
-// Doesn't function on macos
 
-// The purpose of this code is to add programmatically the pattern of the 3 following lines to dart files that are not widgets, not covered by "flutter test",
-// yet are useful to run.
+// On macOS, gets a PathAccessException when using a project root environment variable: JOURNEYERS_DIR
+// Cannot open file, path = '/Users/dev/Desktop/journeyers/test/pages/context_analysis_context_form_page_visual_testing.dart' 
+// (OS Error: Operation not permitted, errno = 1)
 
 // flutter run -t ./qa_utils/utils_manual_testing/flutter_run_comments_util.dart -d linux
 // flutter run -t ./qa_utils/utils_manual_testing/flutter_run_comments_util.dart -d macos
 // flutter run -t ./qa_utils/utils_manual_testing/flutter_run_comments_util.dart -d windows
-
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:journeyers/core/utils/files_and_json/file_utils.dart';
 import 'package:logging/logging.dart';
 
@@ -26,9 +26,14 @@ void main() async
   /// https://docs.flutter.dev/platform-integration/desktop#from-the-command-line
   /// "A path is represented by a number of path components separated by a path separator which is a / on POSIX systems and can be a / or \ on Windows."
   /// https://hackage.haskell.org/package/path-0.9.6/docs/Path-Posix.html#:~:text=A%20path%20is%20represented%20by%20a%20number%20of%20path%20components%20separated%20by%20a%20path%20separator%20which%20is%20a%20/%20on%20POSIX%20systems%20and%20can%20be%20a%20/%20or%20%5C%20on%20Windows.
-    
-  String filePath =  r"test\pages\context_analysis_context_form_page_visual_testing.dart";
-  // String filePath = "test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart";
+  
+  Map<String, String> envVars = Platform.environment;
+  String projectDir = "";
+  if (defaultTargetPlatform == TargetPlatform.macOS) 
+  {projectDir = envVars['JOURNEYERS_DIR']!;}
+
+  String filePath =  "test/pages/context_analysis_context_form_page_visual_testing.dart";
+  // String filePath = "./test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart";
   String commentBegin = "// flutter run -t ";
   String deviceFlag =  " -d ";
   String commentEndChrome = "chrome";
@@ -38,14 +43,23 @@ void main() async
 
   String delimiterLine = "//Line for automated processing";
 
-  File file = File(filePath);
-  String content = file.readAsStringSync();
-  if (content.contains(delimiterLine))
+  File file = File("$projectDir$filePath");
+  if (!file.existsSync()) 
   {
-    logger.shout("The delimiter line was found in $filePath");
-    logger.shout("");
-    // exit(0);    // Gives an impression of program crash
+    logger.shout("File doesn't exist: ${file.path}");
+    logger.shout("Current working directory: ${Directory.current}");
+    exit(0);    
   }
+
+  String content;
+  try
+  {
+    content = file.readAsStringSync();
+    if (content.contains(delimiterLine))
+    {
+      logger.shout("The delimiter line was found in $filePath");
+      logger.shout("");      
+    }
   else 
   {
     logger.info("The delimiter line wasn't found in $filePath");
@@ -64,4 +78,11 @@ void main() async
     logger.info("The comments should have been added.");
     logger.shout("");
   }
+  }
+  on PathAccessException catch(e)
+  {
+    logger.shout("PathAccessException: $e");
+  }
+   
+  
 }
