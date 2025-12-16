6d440ee954b4779b8f3dea72121bf99ccbdb5e96
jl <you@example.com>
2025-12-16 14:14:24 -0600

    Comment added to the custom widgets testing helper code

--- qa_utils/utils_semi_automated_testing/flutter_run_comments_helper.py
+++ qa_utils/utils_semi_automated_testing/flutter_run_comments_helper.py
@@ -30,6 +30,7 @@ def main():
     # Relative file path
     # path on Windows: "test\common_widgets\interaction_and_inputs\custom_checkbox_list_tile_with_text_field_visual_testing.dart"
     # path on macOS: "test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart"
+    # path on Lubuntu: "test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart"
 
     # file_path = r"test\common_widgets\display_and_content\custom_dismissable_rectangular_area_visual_testing.dart"
     file_path = "test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart"
