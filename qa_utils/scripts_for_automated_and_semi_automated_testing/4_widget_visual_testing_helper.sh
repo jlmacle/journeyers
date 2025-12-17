ded751e87ba2a85c26d4878fa4a74afe97b9ee42
jl <you@example.com>
2025-12-17 10:18:29 -0600

    Linux script updated

--- qa_utils/scripts_for_automated_and_semi_automated_testing/4_widget_visual_testing_helper.sh
+++ qa_utils/scripts_for_automated_and_semi_automated_testing/4_widget_visual_testing_helper.sh
@@ -6,11 +6,11 @@ sleep 5
 
 xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart -d web-server --web-port 8091" &
 xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_expansion_tile_visual_testing.dart -d web-server --web-port 8092" &
-xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_snackbar_start_message_helper_visual_testing.dart -d web-server --web-port 8093" &
-xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_dismissable_rectangular_area_visual_testing.dart -d web-server --web-port 8094" &
-xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart -d web-server --web-port 8095" &
-xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_padded_text_field_visual_testing.dart -d web-server --web-port 8096" &
-xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switcher_visual_testing.dart -d web-server --web-port 8097" &
+xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switch_visual_testing.dart -d web-server --web-port 8093" &
+xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_snackbar_start_message_helper_visual_testing.dart -d web-server --web-port 8094" &
+xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_dismissable_rectangular_area_visual_testing.dart -d web-server --web-port 8095" &
+xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart -d web-server --web-port 8096" &
+xterm -e "cd ./; flutter run -t ./test/common_widgets/interaction_and_inputs/custom_padded_text_field_visual_testing.dart -d web-server --web-port 8097" &
 xterm -e "cd ./; flutter run -t ./test/common_widgets/display_and_content/custom_header_visual_testing.dart -d web-server --web-port 8098" &
 
 # Waiting for the web servers to start
