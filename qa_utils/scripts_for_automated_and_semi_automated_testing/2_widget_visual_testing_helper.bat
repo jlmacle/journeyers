a07d9a009e00cbb2246d6fe4749d256a5f420061
unknown <jeanlouis.macle@gmail.com>
2025-12-17 08:48:48 -0600

    Windows script updated

--- qa_utils/scripts_for_automated_and_semi_automated_testing/2_widget_visual_testing_helper.bat
+++ qa_utils/scripts_for_automated_and_semi_automated_testing/2_widget_visual_testing_helper.bat
@@ -14,21 +14,21 @@ timeout /t 5 >nul
 
 
 
-start  flutter run -t ./test/common_widgets/display_and_content/custom_dismissable_rectangular_area_visual_testing.dart  -d web-server --web-port  8091
+start  flutter run -t ./test/common_widgets/display_and_content/custom_header_visual_testing.dart  -d web-server --web-port  8091
 
-start  flutter run -t ./test/common_widgets/display_and_content/custom_header_visual_testing.dart  -d web-server --web-port  8092
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart  -d web-server --web-port  8092
 
-start  flutter run -t ./test/common_widgets/display_and_content/custom_snackbar_start_message_helper_visual_testing.dart  -d web-server --web-port  8093
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_dismissable_rectangular_area_visual_testing.dart  -d web-server --web-port  8093
 
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart  -d web-server --web-port  8094
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_expansion_tile_visual_testing.dart  -d web-server --web-port  8094
 
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_expansion_tile_visual_testing.dart  -d web-server --web-port  8095
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switcher_visual_testing.dart  -d web-server --web-port  8095
 
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switcher_visual_testing.dart  -d web-server --web-port  8096
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_padded_text_field_visual_testing.dart  -d web-server --web-port  8096
 
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_padded_text_field_visual_testing.dart  -d web-server --web-port  8097
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart  -d web-server --web-port  8097
 
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart  -d web-server --web-port  8098
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_snackbar_start_message_helper_visual_testing.dart  -d web-server --web-port  8098
 
 
 
