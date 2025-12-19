79c17eacf9cbebafcaf4db1a094c3ea48e6808b2
unknown <jeanlouis.macle@gmail.com>
2025-12-19 05:23:52 -0600

    Code cleanup

--- test/common_widgets/interaction_and_inputs/custom_language_switch_visual_testing.dart
+++ test/common_widgets/interaction_and_inputs/custom_language_switch_visual_testing.dart
@@ -116,7 +116,6 @@ class _HomePageState extends State<HomePage>
       (
         title: Semantics
         (
-          focused: true,
           focusable: true, 
           child: Focus
           (
