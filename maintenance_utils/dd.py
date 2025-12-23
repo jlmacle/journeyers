9fc86e4c773eca3459d85c6e274e353cdeef887b
unknown <jeanlouis.macle@gmail.com>
2025-12-23 13:18:12 -0600

    Script for dart doc modified output updated

--- maintenance_utils/dd.py
+++ maintenance_utils/dd.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import subprocess
 
 root_folder = os.environ.get('JOURNEYERS_DIR')
@@ -6,9 +7,15 @@ doc_folder = "maintenance_utils/doc"
 os.chdir(root_folder)
 os.makedirs(doc_folder, exist_ok=True)
 
+os_name = platform.system().lower()
+print(f"os_name: {os_name}")
+
 # Running the 'dart doc'command
 # shell=True to avoid 'FileNotFoundError: The system cannot find the file specified'
-result = subprocess.run(['dart', 'doc', '-o', f'{doc_folder}'],shell=True)
+if (os_name == 'windows'):
+    result = subprocess.run(['dart', 'doc', '-o', f'{doc_folder}'],shell=True)
+else:
+    result = subprocess.run(['dart', 'doc', '-o', f'{doc_folder}'],text=True)
 
 print("Output:", result.stdout)
 print("Error:", result.stderr)
