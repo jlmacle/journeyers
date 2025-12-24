90b434318ce101a99994d7b0ee1e3ac898608a2b
unknown <jeanlouis.macle@gmail.com>
2025-12-24 03:42:06 -0600

    Code cleanup

--- lib/common_widgets/interaction_and_inputs/custom_expansion_tile.dart
+++ lib/common_widgets/interaction_and_inputs/custom_expansion_tile.dart
@@ -8,36 +8,39 @@ import '../interaction_and_inputs/custom_icon_button.dart';
 /// A customizable expansion tile.
 class CustomExpansionTile extends StatefulWidget 
 {
-  /// The text to display when the tile is not expanded
+  /// The text to display when the tile is not expanded.
   final String text;
-  /// The font for the text to display when the tile is not expanded
+  /// The font for the text to display when the tile is not expanded.
   final FontWeight textFontWeight;
-  /// The icon used to suggest that the tile is expandable
+  /// The icon used to suggest that the tile is expandable.
   final Icon actionIconSuggestingExpansion;
-  /// The horizontal padding for the expanded content
+  /// The horizontal padding for the expanded content.
   final double expandedContentPaddingHorizontal;
-  /// The vertical padding for the expanded content
+  /// The vertical padding for the expanded content.
   final double expandedContentPaddingVertical;
-  /// The horizontal location of the expanded content
+  /// The horizontal location of the expanded content.
   final CrossAxisAlignment expandedContentCrossAxisAlignment;
-  /// The text displayed in the expanded content
+  /// The text displayed in the expanded content.
   final String expandedAdditionalText;
-  /// The height of a divider under the additional text
+  /// The height of the divider under the additional text.
   final double expandedContentDividerHeight;
-  /// The callback function called to edit from the expansion tile
+  /// The callback function called to edit from the expansion tile.
   final VoidCallback parentWidgetOnEditPressedCallBackFunction;
-  /// The callback function called to delete from the expansion tile
+  /// The callback function called to delete from the expansion tile.
   final VoidCallback parentWidgetOnDeletePressedCallBackFunction;
-  /// The callback function called to share from the expansion tile
+  /// The callback function called to share from the expansion tile.
   final VoidCallback parentWidgetOnSharePressedCallBackFunction;
-  /// A list of  [iconData, toolTipLabel, callBackFunction] values
+  /// A list of  [iconData, toolTipLabel, callBackFunction] values for the action icon(s).
   final List<List<dynamic>> listActionIconsData;
-  /// The horizontal location of the icon(s)
+  /// The horizontal location of the icon(s).
   final MainAxisAlignment listActionIconsMainAxisAlignment;
 
-  static const String editStr = 'Edit';
-  static const String deleteStr = 'Delete';
-  static const String shareStr = 'Share';
+  /// The tooltip label for the "Edit" icon.
+  static const String toolTipEdit = 'Edit';
+  /// The tooltip label for the "Delete" icon.
+  static const String toolTipDelete = 'Delete';
+  /// The tooltip label for the "Share" icon.
+  static const String toolTipShare = 'Share';
 
   const CustomExpansionTile
   ({
@@ -53,9 +56,9 @@ class CustomExpansionTile extends StatefulWidget
     this.listActionIconsMainAxisAlignment = MainAxisAlignment.end,
     this.listActionIconsData = const
     [
-      [Icons.edit, editStr, null],
-      [Icons.delete, deleteStr,null],
-      [Icons.share, shareStr, null],
+      [Icons.edit, toolTipEdit, null],
+      [Icons.delete, toolTipDelete, null],
+      [Icons.share, toolTipShare, null],
     ],
     required this.parentWidgetOnEditPressedCallBackFunction,
     required this.parentWidgetOnDeletePressedCallBackFunction,
@@ -117,15 +120,15 @@ class _CustomExpansionTileState extends State<CustomExpansionTile>
                     final iconData = actionIconData[0] as IconData;
                     final toolTipLabel = actionIconData[1] as String;
                     final VoidCallback onPressedFunction;
-                    if (toolTipLabel == CustomExpansionTile.editStr) 
+                    if (toolTipLabel == CustomExpansionTile.toolTipEdit) 
                     {
                       onPressedFunction = widget.parentWidgetOnEditPressedCallBackFunction;
                     }
-                    else if (toolTipLabel == CustomExpansionTile.deleteStr)
+                    else if (toolTipLabel == CustomExpansionTile.toolTipDelete)
                     {
                       onPressedFunction = widget.parentWidgetOnDeletePressedCallBackFunction;
                     }
-                    else if (toolTipLabel == CustomExpansionTile.shareStr)
+                    else if (toolTipLabel == CustomExpansionTile.toolTipShare)
                     {
                       onPressedFunction = widget.parentWidgetOnSharePressedCallBackFunction;
                     }
