3a20168c4d7da9d24947db33d61e1d3aad3392a0
unknown <jeanlouis.macle@gmail.com>
2025-12-27 05:35:15 -0600

    Code cleanup

--- utils_dev/doc_generator_dart.py
+++ utils_dev/doc_generator_dart.py
@@ -12,7 +12,7 @@ root_folder = os.environ.get('JOURNEYERS_DIR')
 os.chdir(root_folder)
 
 # Doc folder creation if needed
-doc_folder = "utils_maintenance/doc_dart"
+doc_folder = "utils_dev/doc_dart"
 # https://docs.python.org/3/library/os.html#os.makedirs
 # exist_ok=True to avoid an error when the folder already exists.
 os.makedirs(doc_folder, exist_ok=True)
