dfd129776df8b0303e02e2ba9ad03ca3c0d0745f
unknown <jeanlouis.macle@gmail.com>
2025-12-29 10:29:34 -0600

    Code cleanup

--- lib/core/utils/l10n/l10n_utils.dart
+++ lib/core/utils/l10n/l10n_utils.dart
@@ -1,34 +1,35 @@
-import 'package:journeyers/l10n/app_localizations.dart';
-import 'package:flutter/material.dart';
+import 'package:journeyers/l10n/app_localizations.dart'; 
+import 'package:flutter/material.dart'; 
 
-/// {@category Utils}
-/// A utility class related to localization.
+/// {@category Utils} 
+/// A utility class related to localization. 
 class L10nLanguages 
 {  
 
-/// A method used to get a list of all the language names, related to a locale, from the l10n data.
-/// For example, \["Arabic", "Chinese", "English", "French", "Hindi", "Portuguese", "Spanish"\].
-/// When adding a new base locale, file and method can be updated using l10n_utils_update.py.
+/// A method used to get a list of all the language names, related to a locale, from the l10n data. 
+/// For example, \["Arabic", "Chinese", "English", "French", "Hindi", "Portuguese", "Spanish"\]. 
+/// When adding a new base locale, file and method can be updated using l10n_utils_update.py. 
+//  Note:  \[ and \] for dart doc 
 static List<String> getLanguages(BuildContext context) 
 {
-  List<String> languages = [];
-  // Code to generate automatically from the base locales l10n data: begin
+  List<String> languages = []; 
+  // Code to generate automatically from the base locales l10n data: begin 
   languages.add(AppLocalizations.of(context)?.lang_en ?? 'Default for "en" language');
   languages.add(AppLocalizations.of(context)?.lang_fr ?? 'Default for "fr" language');
-  // Code to generate automatically from the base locales l10n data: end
-  languages.sort();
-  return languages;
+  // Code to generate automatically from the base locales l10n data: end 
+  languages.sort(); 
+  return languages; 
 }
 
-/// A method used to get a language code, being provided a language name.
-static String? getLangCodeFromLangName(String langName)
-{
-  // Code to generate automatically from the base locales l10n data: begin
-  List<String> enLanguage = ["English","Anglais"];
-  List<String> frLanguage = ["French","Français"];
-  if (enLanguage.contains(langName)) return 'en';
-  if (frLanguage.contains(langName)) return 'fr';
-  // Code to generate automatically from the base locales l10n data: end
-  return null;
+/// A method used to get a language code, being provided a language name. 
+static String? getLangCodeFromLangName(String langName) 
+{ 
+  // Code to generate automatically from the base locales l10n data: begin 
+  List<String> enLanguage = ["English","Anglais"]; 
+  List<String> frLanguage = ["French","Français"]; 
+  if (enLanguage.contains(langName)) return 'en'; 
+  if (frLanguage.contains(langName)) return 'fr'; 
+  // Code to generate automatically from the base locales l10n data: end 
+  return null; 
 }
 }
