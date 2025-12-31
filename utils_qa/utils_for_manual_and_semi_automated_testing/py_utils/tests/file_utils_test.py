533680b34c6495ae98e49c9c6950e5112d3be10d
unknown <jeanlouis.macle@gmail.com>
2025-12-31 02:17:00 -0600

    Code cleanup

---
+++ utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/file_utils_test.py
@@ -0,0 +1,19 @@
+import platform
+from pathlib import WindowsPath, PosixPath
+
+from py_utils.file_utils import *
+
+# Getting the operating system name
+os_name = platform.system().lower()
+
+def test_get_files_in_directory():
+    directory_path = "utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data"
+    file_extension = ".arb"
+    expected = []
+
+    if os_name.startswith('windows'):
+        expected = [WindowsPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_en.arb'), WindowsPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_en_US.arb'), WindowsPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_fr.arb'), WindowsPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_fr_FR.arb')]
+    if os_name.startswith('linux') or os_name.startswith('darwin'):        
+        expected = [PosixPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_en.arb'), PosixPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_en_US.arb'), PosixPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_fr.arb'), PosixPath('utils_qa/utils_for_manual_and_semi_automated_testing/py_utils/tests/arb_utils_test_data/app_fr_FR.arb')]
+
+    assert set(get_files_in_directory(directory_path=directory_path, file_extension=file_extension)) == set(expected)
