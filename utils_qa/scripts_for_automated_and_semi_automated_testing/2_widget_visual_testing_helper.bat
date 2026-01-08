fb65a102b7fc2a2cb9665284df05e69724da61d6
unknown <jeanlouis.macle@gmail.com>
2026-01-08 08:56:18 -0600

    Code cleanup

--- utils_qa/scripts_for_automated_and_semi_automated_testing/2_widget_visual_testing_helper.bat
+++ utils_qa/scripts_for_automated_and_semi_automated_testing/2_widget_visual_testing_helper.bat
@@ -1,24 +1,48 @@
-:: Please note that Chrome must be started to have more than one tab launched.
-:: Batch file launching the widgets, testing the custom widgets, in Chrome tabs.
-@echo off
-set BROWSER="C:\Program Files\Google\Chrome\Application\chrome.exe"
-cd ../..
-echo "After launching the terminals, programm to wait for the web servers to be completely started before opening the browser tabs"
-timeout /t 5 >nul
-
-start  flutter run -t ./test/common_widgets/display_and_content/custom_focusable_text_visual_testing.dart  -d web-server --web-port  8091
-start  flutter run -t ./test/common_widgets/display_and_content/custom_heading_visual_testing.dart  -d web-server --web-port  8092
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart  -d web-server --web-port  8093
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_expansion_tile_visual_testing.dart  -d web-server --web-port  8094
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switch_visual_testing.dart  -d web-server --web-port  8095
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_padded_text_field_visual_testing.dart  -d web-server --web-port  8096
-start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart  -d web-server --web-port  8097
-:: Waiting for the web servers to start
-timeout /t 70 >nul 
-%BROWSER% http://localhost:8091
-%BROWSER% http://localhost:8092
-%BROWSER% http://localhost:8093
-%BROWSER% http://localhost:8094
-%BROWSER% http://localhost:8095
-%BROWSER% http://localhost:8096
-%BROWSER% http://localhost:8097
+:: Please note that Chrome must be started to have more than one tab launched.
+
+:: Batch file launching the widgets, testing the custom widgets, in Chrome tabs.
+
+@echo off
+
+set BROWSER="C:\Program Files\Google\Chrome\Application\chrome.exe"
+
+cd ../..custom_widgets
+custom_widgets
+echo "After launching the tercustom_widgetsmm to wait for the web servers to be completely started before opening the browser tabs"
+custom_widgets
+timeout /t 5 >nulcustom_widgets
+custom_widgets
+custom_widgets
+
+start  flutter run -t ./test/common_widgets/display_and_content/custom_focusable_text_visual_testing.dart  -d web-server --web-port  8091
+
+start  flutter run -t ./test/common_widgets/display_and_content/custom_heading_visual_testing.dart  -d web-server --web-port  8092
+
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart  -d web-server --web-port  8093
+
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_expansion_tile_visual_testing.dart  -d web-server --web-port  8094
+
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_language_switch_visual_testing.dart  -d web-server --web-port  8095
+
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_padded_text_field_visual_testing.dart  -d web-server --web-port  8096
+
+start  flutter run -t ./test/common_widgets/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart  -d web-server --web-port  8097
+
+:: Waiting for the web servers to start
+
+timeout /t 70 >nul 
+
+%BROWSER% http://localhost:8091
+
+%BROWSER% http://localhost:8092
+
+%BROWSER% http://localhost:8093
+
+%BROWSER% http://localhost:8094
+
+%BROWSER% http://localhost:8095
+
+%BROWSER% http://localhost:8096
+
+%BROWSER% http://localhost:8097
+
