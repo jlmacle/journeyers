86f3864aa460d62dffc994d635fec99264fe091d
unknown <jeanlouis.macle@gmail.com>
2026-01-27 06:08:39 -0600

    Code added to store if context analysis session data has been saved

--- lib/core/utils/settings_and_preferences/user_preferences_utils.dart
+++ lib/core/utils/settings_and_preferences/user_preferences_utils.dart
@@ -2,16 +2,33 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 /// {@category Utils}
 /// A utility class related to user preferences.
-class UserPreferencesUtils {
+class UserPreferencesUtils 
+{
   /// Method used to record that the start message has been acknowledged.
-  Future<void> saveStartMessageAcknowledgement() async {
+  Future<void> saveStartMessageAcknowledgement() async 
+  {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isDialogStartMessageAcknowledged', true);
   }
 
   /// Method used to check if the start message has been acknowledged.
-  Future<bool> isStartMessageAcknowledged() async {
+  Future<bool> isStartMessageAcknowledged() async 
+  {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getBool('isDialogStartMessageAcknowledged') ?? false;
+  }
+
+  /// Method used to record that session data has been saved.
+  Future<void> saveSessionDataHasBeenSaved() async 
+  {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('wasSessionDataSaved', true);
+  }
+
+  /// Method used to check if session data has been saved.
+  Future<bool> wasSessionDataSaved() async 
+  {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('wasSessionDataSaved') ?? false;
   }
 }
