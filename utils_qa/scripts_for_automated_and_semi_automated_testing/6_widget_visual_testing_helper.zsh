0a3fd9d07bb5994ed34891e88306abf592772839
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2026-02-04 09:49:59 +0100

    Code cleanup

--- utils_qa/scripts_for_automated_and_semi_automated_testing/6_widget_visual_testing_helper.zsh
+++ utils_qa/scripts_for_automated_and_semi_automated_testing/6_widget_visual_testing_helper.zsh
@@ -4,5 +4,19 @@ cd ../..
 echo "After launching the terminals, programm to wait for the web servers to be completely started before opening the browser tabs"
 sleep 5
 
+osascript -e 'tell application "Terminal" to do script "cd /Users/dev/Desktop/journeyers/; flutter run -t ./test/widgets/custom/interaction_and_inputs/custom_checkbox_list_tile_with_text_field_visual_testing.dart -d web-server --web-port 8091"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/dev/Desktop/journeyers/; flutter run -t ./test/widgets/custom/interaction_and_inputs/custom_padded_text_field_visual_testing.dart -d web-server --web-port 8092"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/dev/Desktop/journeyers/; flutter run -t ./test/widgets/custom/interaction_and_inputs/custom_language_switch_visual_testing.dart -d web-server --web-port 8093"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/dev/Desktop/journeyers/; flutter run -t ./test/widgets/custom/interaction_and_inputs/custom_expansion_tile_visual_testing.dart -d web-server --web-port 8094"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/dev/Desktop/journeyers/; flutter run -t ./test/widgets/custom/interaction_and_inputs/custom_segmented_button_with_text_field_visual_testing.dart -d web-server --web-port 8095"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/dev/Desktop/journeyers/; flutter run -t ./test/widgets/custom/text/custom_focusable_text_visual_testing.dart -d web-server --web-port 8096"'
+osascript -e 'tell application "Terminal" to do script "cd /Users/dev/Desktop/journeyers/; flutter run -t ./test/widgets/custom/text/custom_heading_visual_testing.dart -d web-server --web-port 8097"'
 # Waiting for the web servers to start
 sleep 70 
+open -a "Google Chrome" "http://localhost:8091"
+open -a "Google Chrome" "http://localhost:8092"
+open -a "Google Chrome" "http://localhost:8093"
+open -a "Google Chrome" "http://localhost:8094"
+open -a "Google Chrome" "http://localhost:8095"
+open -a "Google Chrome" "http://localhost:8096"
+open -a "Google Chrome" "http://localhost:8097"
