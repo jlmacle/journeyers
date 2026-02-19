4cc775af2d087df58d811250c27aa990a9f4b565
unknown <jeanlouis.macle@gmail.com>
2026-02-19 15:52:12 +0100

    Integration testing on Android

---
+++ android/integration_test/externalized_code/integration_test_utils.dart
@@ -0,0 +1,82 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:journeyers/core/utils/printing_and_logging/print_utils.dart';
+
+PrintUtils _pu = PrintUtils();
+
+/// Method used to retrieve the index of a session data, knowing its title
+int? getSessionDataIndexFromTitle({required List<Map<String, dynamic>> sessionData, required String title})
+{
+  int? index;
+
+  int i = 0;
+  for (var dataMap in sessionData)
+  {
+    if (dataMap["title"] == title)
+    {
+      index = i;
+    }
+    else { i += 1;}
+  }
+  return index;
+}
+
+/// Method used to list all the sessions titles
+Future<List<String>> getSessionsTitlesList({required WidgetTester tester, required List<Map<String, dynamic>> sessionData, required String keyRoot}) async
+{
+  List<String> sessionsTitlesList = [];
+  String currentTitle = "";
+
+  for (int index = 0; index < sessionData.length; index++) 
+  {
+    // Getting the finder
+    final titleFinder = find.byKey(ValueKey('$keyRoot$index'));
+      // To have all the elements visible, therefore all the titles accessed
+    await tester.ensureVisible(titleFinder); 
+    await tester.pump();
+    
+    // Verifying the finder existence to get the loading complete
+    expect(titleFinder, findsOneWidget);
+
+    // Extracting the actual widget instance from the tester
+    final Text titleWidget = tester.widget(titleFinder);            
+
+    //Accessing the widget properties
+    currentTitle = titleWidget.data!;
+    sessionsTitlesList.add(currentTitle);
+    _pu.printd('Title: ${titleWidget.data}');
+  }
+  return sessionsTitlesList;
+}
+
+/// Method used to test if the test data titles are already present in the pre-test sessions titles
+bool isThereATestDataTitleDuplicated({
+  required List<String> listOfSessionTitles, 
+  required List<String> listOfTestDataTitles,
+}) {
+  return listOfTestDataTitles.any((String title) {
+    final int occurrences = listOfSessionTitles
+        .where((String s) => s == title)
+        .length;
+        
+    return occurrences >= 2;
+  });
+}
+
+/// Method used to scroll up the screen (-200 as default delta to go up the list)
+Future<int> scrollListUpScreen({required WidgetTester tester, required Finder listFinder, required elementToReachFinder , double delta = -200}) async
+{
+  await tester.scrollUntilVisible
+  (
+    elementToReachFinder, 
+    -200 , // getting back up the list
+    scrollable:find.descendant
+    (
+      of: listFinder,
+      matching: find.byType(Scrollable),
+    )
+  );
+  return await tester.pumpAndSettle(); 
+}
