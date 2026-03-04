8e7bef4a02758f578f0632a9336cb02cd62dd2cd
unknown <jeanlouis.macle@gmail.com>
2026-03-04 12:05:16 +0100

    Code cleanup

---
+++ lib/pages/context_analysis/context_analysis_form_widgets/context_analysis_file_name_desktop_platforms.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:journeyers/app_themes.dart';
+
+import 'package:journeyers/core/utils/printing_and_logging/print_utils.dart';
+import 'package:journeyers/core/utils/settings_and_preferences/user_preferences_utils.dart';
+import 'package:journeyers/pages/context_analysis/context_analysis_form_page.dart';
+
+//**************** UTILITY CLASS ****************//
+PrintUtils pu = PrintUtils();
+UserPreferencesUtils upu = UserPreferencesUtils(); 
+
+// StatefulWidget necessary for overriding dispose() 
+
+/// {@category Pages}
+/// {@category Context analysis}
+/// Defining file name and saving file for the context analysis, on desktop platforms.
+class ContextAnalysisFileNameDesktopPlatforms extends StatefulWidget 
+{
+  /// A global key for the context analysis form page
+  final GlobalKey<ContextAnalysisFormPageState> contextAnalysisFormPageKey;
+
+  const ContextAnalysisFileNameDesktopPlatforms
+  ({
+    super.key,
+    required this.contextAnalysisFormPageKey,
+  });
+
+  @override
+  State<ContextAnalysisFileNameDesktopPlatforms> createState() => _ContextAnalysisFileNameDesktopPlatformsState();
+}
+
+class _ContextAnalysisFileNameDesktopPlatformsState extends State<ContextAnalysisFileNameDesktopPlatforms> 
+{
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+    ElevatedButton
+    ( 
+      onPressed: widget.contextAnalysisFormPageKey.currentState?.print2CSV,
+      child: Text(
+        'Click to save your data in CSV, \nspreadsheet-compatible format',
+        style: elevatedButtonTextStyle,
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
