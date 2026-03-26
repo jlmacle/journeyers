b897928891026198bd347341136e56c1a17300ea
unknown <jeanlouis.macle@gmail.com>
2026-03-26 05:34:24 -0500

    Feature added for editing the problem to be solved

--- lib/pages/group_problem_solving/group_problem_solving_widgets/problem_to_solve.dart
+++ lib/pages/group_problem_solving/group_problem_solving_widgets/problem_to_solve.dart
@@ -1,17 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:journeyers/app_themes.dart';
 
-class ProblemToSolve extends StatelessWidget {
+class ProblemToSolve extends StatefulWidget {
   const ProblemToSolve({super.key});
 
   @override
+  State<ProblemToSolve> createState() => _ProblemToSolveState();
+}
+
+class _ProblemToSolveState extends State<ProblemToSolve> {
+  final TextEditingController _controller = TextEditingController(text: 'Problem to solve');
+  bool _isEditing = false;
+
+  @override
+  void dispose() {
+    _controller.dispose(); 
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return 
-    const Padding
-    (
-      padding: EdgeInsetsGeometry.all(10),
-      // Should be a text field becoming a text
-      child:Center(child: Text('Problem to solve', style:dialogStyle))
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Center(
+        child: _isEditing 
+          ? TextField(
+              controller: _controller,
+              autofocus: true,
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.check, color: greenShade900),
+                  onPressed: () => setState(() => _isEditing = false),
+                ),
+              ),
+              onSubmitted: (_) => setState(() => _isEditing = false),
+            )
+          : 
+          Flex(
+          direction: Axis.horizontal, 
+          children: [
+            // Widget 1: Left side
+            Container(width: 50),
+            
+            // Widget 2: The Centered Text
+            // Expanded fills the middle gap so Center can work effectively
+            const Expanded(
+              child: Center(
+                child: Text(
+                  'Problem to solve',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            
+            // Widget 3: Right side
+            Container(
+              width: 50,
+              child: GestureDetector
+              (
+                child: const Text("✏️"),
+                onTap: () => setState(() => _isEditing = true),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
