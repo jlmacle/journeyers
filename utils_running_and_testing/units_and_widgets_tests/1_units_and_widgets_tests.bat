e428abec21deaf166c0aa8fa90f988f854f87519
unknown <jeanlouis.macle@gmail.com>
2026-04-09 09:58:01 -0500

    Testing script added

--- utils_running_and_testing/units_and_widgets_tests/1_units_and_widgets_tests.bat
+++ utils_running_and_testing/units_and_widgets_tests/1_units_and_widgets_tests.bat
@@ -1,3 +1,10 @@
-cd ../..
+@echo off
+
+echo ---------------------------------------------  
+echo Unit and widget tests.
+echo ---------------------------------------------
+echo JOURNEYERS_DIR must be set to the project root directory
+
+cd %JOURNEYERS_DIR%
 flutter test -r github
 
