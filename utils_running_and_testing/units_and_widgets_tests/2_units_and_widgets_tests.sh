41e3e43d160d55485a28812b373660011e5f20bb
jl <you@example.com>
2026-04-09 10:16:09 -0500

    Testing script added

--- utils_running_and_testing/units_and_widgets_tests/2_units_and_widgets_tests.sh
+++ utils_running_and_testing/units_and_widgets_tests/2_units_and_widgets_tests.sh
@@ -1,4 +1,9 @@
-# chmod u+x 2_automated_tests.sh
+# chmod u+x 2_units_and_widgets_tests.sh
 
-cd ../..
+echo "---------------------------------------------"  
+echo "Unit and widget tests."
+echo "---------------------------------------------"
+echo "JOURNEYERS_DIR must be set to the project root directory"
+
+cd $JOURNEYERS_DIR
 flutter test -r github
