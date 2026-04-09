5342b5e3c53d83c88f4066172772c92ecba7756d
unknown <jeanlouis.macle@gmail.com>
2026-04-09 10:00:30 -0500

    Testing script added

---
+++ utils_running_and_testing/integration_tests/2_Windows_integration_tests.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ---------------------------------------------  
+echo Integration tests.
+echo ---------------------------------------------
+echo JOURNEYERS_DIR must be set to the project root directory
+
+cd %JOURNEYERS_DIR%
+flutter test ./integration_test/_all_tests.dart  -r github -d windows
+
