ce88bf2b0be4beb78430a9d5f69bbcbae794bb3b
unknown <jeanlouis.macle@gmail.com>
2026-04-09 10:23:01 -0500

    Code cleanup

--- utils_running_and_testing/all_tests/5_Windows_all_tests.bat
+++ utils_running_and_testing/all_tests/5_Windows_all_tests.bat
@@ -1,8 +1,16 @@
+@echo off
+echo ---------------------------------------------  
+echo All tests.
+echo ---------------------------------------------
+echo JOURNEYERS_DIR must be set to the project root directory
+
+cd %JOURNEYERS_DIR%
+
 :: Unit and widget tests
-cd ../units_and_widgets_tests
+cd ./utils_running_and_testing/units_and_widgets_tests
 call ./1_units_and_widgets_tests.bat
 
 :: Integration tests
 cd ./utils_running_and_testing/integration_tests
-call ./2_Windows_integration_tests.bat
+call ./5_Windows_integration_tests.bat
 
