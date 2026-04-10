be7300325c3769102a48fe0ecc389bb4b802b868
unknown <jeanlouis.macle@gmail.com>
2026-04-10 09:15:41 -0500

    Custom text field widget with validation/error message

--- lib/widgets/custom/interaction_and_inputs/text_field_checked.dart
+++ lib/widgets/custom/interaction_and_inputs/text_field_checked.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:journeyers/utils/generic/dev/type_defs.dart';
+
 /// {@category Custom widgets}
 /// A text field that checks the entered value with different functions,
 /// and prevents the value from being submitted, in any of the functions returns true.
@@ -14,8 +16,18 @@ class TextFieldChecked extends StatefulWidget
   /// The style for the hint text.
   final TextStyle textFieldHintStyle;
 
+  /// A key for the error message.
+  final Key errorMessageKey;
+
+  /// The style for the error message.
+  final TextStyle errorMessageStyle;
+
+  /// A callback function called when submitting the value.
+  final ValueChanged<String> valueSubmittedCallbackFunction;
+
   /// A map with functions as keys, and error messages as values.
-  final Map<Function, String> functionErrorMessageMapping;
+  /// The functions return true on a valid input, and false on an invalid input.
+  final Map<StringValidator, String> blockingFunctionsErrorMessagesMapping;
 
   const TextFieldChecked
   ({
@@ -23,7 +35,10 @@ class TextFieldChecked extends StatefulWidget
     required this.textFieldStyle,
     required this.textFieldHint,
     required this.textFieldHintStyle,
-    required this.functionErrorMessageMapping    
+    required this.errorMessageKey,
+    required this.errorMessageStyle,
+    required this.valueSubmittedCallbackFunction,
+    required this.blockingFunctionsErrorMessagesMapping    
   });
 
   @override
@@ -32,8 +47,33 @@ class TextFieldChecked extends StatefulWidget
 
 class _TextFieldCheckedState extends State<TextFieldChecked> 
 {
+  String _errorMessage = "";
+  bool submitIsBlocked = false;
 
+  onChanged(String newValue)
+  {
+    for (final blockingFunction in widget.blockingFunctionsErrorMessagesMapping.keys)
+    {
+      if (blockingFunction(newValue))
+      {
+        // Blocking the submit
+        submitIsBlocked = true;
 
+        // Rendering the error message
+        setState(() {
+          _errorMessage = widget.blockingFunctionsErrorMessagesMapping[blockingFunction]!;
+        });
+      }
+    }
+  }
+
+  onSubmitted(String newValue)
+  {
+    if (!submitIsBlocked)
+    {
+      widget.valueSubmittedCallbackFunction(newValue);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,10 +92,19 @@ class _TextFieldCheckedState extends State<TextFieldChecked>
                 style: widget.textFieldHintStyle
               )
           ),
-          error: Center(key: errorMessageKey, child: Text(textAlign: TextAlign.center, _errorMessageForFileName , style: analysisTextFieldErrorStyle)),
+          error: 
+          Center
+          (
+            key: widget.errorMessageKey, 
+            child: Text
+            (
+              textAlign: TextAlign.center, 
+              _errorMessage, 
+              style: widget.errorMessageStyle)),
           errorMaxLines: 3
       ),
-      onChanged: (_){},
+      onChanged: onChanged,
+      onSubmitted: onSubmitted
     );
   }
 }
