7ee312f505ba73872dec37a46dc37d3014ec99ac
unknown <jeanlouis.macle@gmail.com>
2026-04-11 11:26:01 -0500

    Code cleanup

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -13,7 +13,6 @@ import 'package:journeyers/utils/generic/l10n/l10n_utils.dart';
 import 'package:journeyers/l10n/app_localizations.dart';
 import 'package:journeyers/pages/context_analysis/context_analysis_page.dart';
 import 'package:journeyers/pages/group_problem_solving/group_problem_solving_page.dart';
-import 'package:journeyers/widgets/custom/interaction_and_inputs/custom_language_switch.dart';
 
 /// {@category Pages}
 /// The homepage for the app.
