e40d708a26752cd8549147b0da4caabdf55a366d
unknown <jeanlouis.macle@gmail.com>
2026-04-12 05:49:37 -0500

    Code cleanup

--- lib/utils/project_specific/text_fields/text_field_utils.dart
+++ lib/utils/project_specific/text_fields/text_field_utils.dart
@@ -5,13 +5,19 @@ import 'package:journeyers/utils/generic/text_fields/text_field_utils.dart' as t
 /// A project-specific utility class related to text fields.
 class TextFieldUtils
  { 
-
   //*********************  MAP WITH STRING SANITIZER BUNDLES AS KEYS, AND ERROR MESSAGES AS VALUES *********************//
   
-  /// A map with String sanitizer bundles as keys, and error messages as values.
+  /// A map with String sanitizer bundles as keys, and error messages as values,
+  /// for the context analysis text fields.
   static const Map<StringSanitizerBundle, String> stringSanitizerBundlesErrorsMapForCA = 
   {
     tfu_gen.TextFieldUtils.containsAStraightQuote : tfu_gen.TextFieldUtils.containsAStraightQuoteError,
-    tfu_gen.TextFieldUtils.containsADot : tfu_gen.TextFieldUtils.containsADotError
   };  
+
+  /// A map with String sanitizer bundles as keys, and error messages as values,
+  /// for the file names used on mobile platforms.
+  static const Map<StringSanitizerBundle, String> stringSanitizerBundlesErrorsMapForFileNames = 
+  {
+    tfu_gen.TextFieldUtils.containsADot : tfu_gen.TextFieldUtils.containsADotError
+  }; 
 }
