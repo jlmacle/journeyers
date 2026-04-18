610ea6b082c5a72effa2edcab3d899b39deaff7f
unknown <jeanlouis.macle@gmail.com>
2026-04-18 03:22:33 -0500

    Code cleanup

--- test/widgets/utility/dashboard_widgets/dashboard_filtering_feature_test.dart
+++ test/widgets/utility/dashboard_widgets/dashboard_filtering_feature_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:journeyers/utils/generic/dashboard/dashboard_utils.dart';
 import 'package:journeyers/utils/project_specific/dashboard/dashboard_strings.dart';
 
-import 'package:journeyers/widgets/utility/dashboard_widgets/dashboard_filtering_and_sorting_feature.dart';
-import 'package:journeyers/widgets/utility/dashboard_widgets/dashboard_sorting_by_date.dart';
-import 'package:journeyers/widgets/utility/dashboard_widgets/dashboard_sorting_by_title.dart';
-import 'package:journeyers/widgets/utility/dashboard_widgets/dashboard_filtering_by_keywords.dart';
+import 'package:journeyers/widgets/utility/dashboard_widgets/2_dashboard_filtering_and_sorting_feature.dart';
+import 'package:journeyers/widgets/utility/dashboard_widgets/2b_dashboard_sorting_by_date.dart';
+import 'package:journeyers/widgets/utility/dashboard_widgets/2a_dashboard_sorting_by_title.dart';
+import 'package:journeyers/widgets/utility/dashboard_widgets/2c_dashboard_filtering_by_keywords.dart';
 
 void main() {
   group('DashboardFilteringFeature Widget Tests:\n', () {
