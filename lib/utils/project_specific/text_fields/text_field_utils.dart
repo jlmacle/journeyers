83334b9e97c64d8f9e86bb6964d5a661bbdfe751
unknown <jeanlouis.macle@gmail.com>
2026-04-22 03:06:35 -0500

    Code cleanup

--- lib/utils/project_specific/text_fields/text_field_utils.dart
+++ lib/utils/project_specific/text_fields/text_field_utils.dart
@@ -5,8 +5,8 @@ import 'package:journeyers/utils/generic/text_fields/text_field_utils.dart' as t
 /// A project-specific utility class related to text fields.
 class TextFieldUtils
  { 
-  //*********************  MAP WITH STRING SANITIZER BUNDLES AS KEYS, AND ERROR MESSAGES AS VALUES *********************//
-  
+  // ─── MAP WITH STRING SANITIZER BUNDLES AS KEYS, AND ERROR MESSAGES AS VALUES ───────────────────────────────────────
+ 
   /// A map with String sanitizer bundles as keys, and error messages as values,
   /// for the context analysis text fields.
   static const Map<StringSanitizerBundle, String> stringSanitizerBundlesErrorsMappingForCA = 
@@ -21,8 +21,8 @@ class TextFieldUtils
     tfu_gen.TextFieldUtils.containsADot : tfu_gen.TextFieldUtils.errorContainsADot
   }; 
 
-  //*********************  MAP WITH BLACKLISTING FUNCTIONS AS KEYS, AND ERROR MESSAGES AS VALUES *********************//
-
+  // ─── MAP WITH BLACKLISTING FUNCTIONS AS KEYS, AND ERROR MESSAGES AS VALUES ───────────────────────────────────────
+  
   /// A map with blacklisting functions for CSV files as keys, and error messages as values.
   static const Map<BlacklistingFunction, String> blacklistingFunctionsErrorsMappingForCSVFileNames = 
   {
