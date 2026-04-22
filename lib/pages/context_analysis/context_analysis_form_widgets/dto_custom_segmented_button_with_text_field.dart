23f90d566494cf77955410889b0a719103eb059e
unknown <jeanlouis.macle@gmail.com>
2026-04-22 04:03:38 -0500

    Code cleanup

--- lib/pages/context_analysis/context_analysis_form_widgets/dto_custom_segmented_button_with_text_field.dart
+++ lib/pages/context_analysis/context_analysis_form_widgets/dto_custom_segmented_button_with_text_field.dart
@@ -1,7 +1,11 @@
 /// {@category Context analysis}
 /// A DTO for the SegmentedButtonWithTextField widget.
-class DTOSegmentedButtonWithTextField {
-   Set<String> selection;
+class DTOSegmentedButtonWithTextField 
+{
+  /// The selection of the segmented button.
+  Set<String> selection;
+  /// The text of the text field.
   String text;
+
   DTOSegmentedButtonWithTextField() : selection = {}, text = '';
 }
