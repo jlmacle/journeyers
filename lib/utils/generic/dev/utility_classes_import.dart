f50d90382047909ff1c30f296d0ff692e34e61e0
unknown <jeanlouis.macle@gmail.com>
2026-04-23 09:16:06 -0500

    Code cleanup

--- lib/utils/generic/dev/utility_classes_import.dart
+++ lib/utils/generic/dev/utility_classes_import.dart
@@ -3,11 +3,9 @@ import 'package:journeyers/utils/generic/dashboard/dashboard_utils.dart';
 import 'package:journeyers/utils/generic/files/files_utils.dart';
 import 'package:journeyers/utils/generic/printing_and_logging/print_utils.dart';
 import 'package:journeyers/utils/generic/runtime_data/runtime_data_utils.dart';
-import 'package:journeyers/utils/project_specific/csv/csv_utils.dart';
 
 // ─── UTILITY CLASSES ───────────────────────────────────────
 final CollectionsUtils cu = .new();
-final CSVUtils csvu = .new();
 final DashboardUtils du = .new();
 final FileUtils fu = .new();
 final PrintUtils pu = .new();
