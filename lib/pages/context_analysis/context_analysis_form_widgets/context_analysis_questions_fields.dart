08b67b9c93bc355866543df8121e4ad895983ee1
unknown <jeanlouis.macle@gmail.com>
2026-04-24 05:16:37 -0500

    Code cleanup

--- lib/pages/context_analysis/context_analysis_form_widgets/context_analysis_questions_fields.dart
+++ lib/pages/context_analysis/context_analysis_form_widgets/context_analysis_questions_fields.dart
@@ -6,64 +6,64 @@ class CAQuestionsFields
   // ─── USED IN THE CA FORM ───────────────────────────────────────
   // ─── QUESTIONS: INDIVIDUAL PERSPECTIVE ───────────────────────────────────────
   // Heading level 2
-  /// As an individual: What problem am I trying to solve?
+  /// Question asked in the form: As an individual: What problem am I trying to solve?
   String level2TitleIndividual = 'As an individual: What problem am I trying to solve?';
 
   // Heading level 3 and sub items
-  /// A Balance Issue?
+  /// Question asked in the form: A Balance Issue?
   String level3TitleBalanceIssue = 'A Balance Issue?';
-  /// To balance studies and household life?
+  /// Question asked in the form: To balance studies and household life?
   String level3TitleBalanceIssueItem1 = 'To balance studies and household life?';
-  /// To balance accessing income and household life?
+  /// Question asked in the form: To balance accessing income and household life?
   String level3TitleBalanceIssueItem2 = 'To balance accessing income and household life?';
-  /// To balance earning an income and household life?
+  /// Question asked in the form: To balance earning an income and household life?
   String level3TitleBalanceIssueItem3 = 'To balance earning an income and household life?';
-  /// To balance helping others and household life?
+  /// Question asked in the form: To balance helping others and household life?
   String level3TitleBalanceIssueItem4 = 'To balance helping others and household life?';
 
   // Heading level 3 and sub items
-  /// A Workplace Issue?
+  /// Question asked in the form: A Workplace Issue?
   String level3TitleWorkplaceIssue = 'A Workplace Issue?';
-  /// To solve a need to be more appreciated at work?
+  /// Question asked in the form: To solve a need to be more appreciated at work?
   String level3TitleWorkplaceIssueItem1 = 'To solve a need to be more appreciated at work?';
-  /// To solve a need to remain appreciated at work?
+  /// Question asked in the form: To solve a need to remain appreciated at work?
   String level3TitleWorkplaceIssueItem2 = 'To solve a need to remain appreciated at work?';
 
   // Heading level 3 and sub items
-  /// A Legacy Issue?
+  /// Question asked in the form: A Legacy Issue?
   String level3TitleLegacyIssue = 'A Legacy Issue?';
-  /// To have better legacies to leave to our children/others?
+  /// Question asked in the form: To have better legacies to leave to our children/others?
   String level3TitleLegacyIssueItem1 = 'To have better legacies to leave to our children/others?';
 
   // Heading level 3 without sub items
-  /// Is the issue of another type?
+  /// Question asked in the form: Is the issue of another type?
   String level3TitleAnotherIssue = 'Is the issue of another type?';
 
 
 
   // ─── QUESTIONS: GROUP/TEAM PERSPECTIVE ───────────────────────────────────────
   // Heading level 2
-  /// As a member of groups/teams: What problem(s) are we trying to solve?
+  /// Question asked in the form: As a member of groups/teams: What problem(s) are we trying to solve?
   String level2TitleGroup = 'As a member of groups/teams: What problem(s) are we trying to solve?';
 
   // Heading level 3 without sub items
-  /// What problem(s) are the groups/teams trying to solve?
+  /// Question asked in the form: What problem(s) are the groups/teams trying to solve?
   String level3TitleGroupsProblematics = 'What problem(s) are the groups/teams trying to solve?';
 
   // Heading level 3 without sub items
-  /// Am I trying to solve the same problem(s) as my groups/teams?
+  /// Question asked in the form: Am I trying to solve the same problem(s) as my groups/teams?
   String level3TitleSameProblem = 'Am I trying to solve the same problem(s) as my groups/teams?';
 
   // Heading level 3 without sub items
-  /// Is entering the group problem-solving process consistent with harmony at home?
+  /// Question asked in the form: Is entering the group problem-solving process consistent with harmony at home?
   String level3TitleHarmonyAtHome = 'Is entering the group problem-solving process consistent with harmony at home?';
 
   // Heading level 3 without sub items
-  /// Is entering the group problem-solving process consistent with appreciability at work?
+  /// Question asked in the form: Is entering the group problem-solving process consistent with appreciability at work?
   String level3TitleAppreciabilityAtWork = 'Is entering the group problem-solving process consistent with appreciability at work?';
 
   // Heading level 3 without sub items
-  /// Is entering the group problem-solving process consistent with my income earning ability?
+  /// Question asked in the form: Is entering the group problem-solving process consistent with my income earning ability?
   String level3TitleIncomeEarningAbility = 'Is entering the group problem-solving process consistent with my income earning ability?';
 
 
