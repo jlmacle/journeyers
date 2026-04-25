21014d60490a32918ba7827f30a65ee71d8f0f77
unknown <jeanlouis.macle@gmail.com>
2026-04-25 05:54:45 -0500

    Code cleanup

--- lib/utils/generic/collections/collections_utils.dart
+++ lib/utils/generic/collections/collections_utils.dart
@@ -2,7 +2,7 @@
 /// A generic utility class related to collections of items.
 class CollectionsUtils 
 {
-  /// Method used to test that two lists have identical sets of elements
+  /// Method used to test that two lists have identical sets of elements.
   bool areListsEqualSets(List list1, List list2) 
   {
     final set1 = Set.from(list1);
