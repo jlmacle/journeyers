ab8a775cf5902cc30c52454343152bca0ca27c63
unknown <jeanlouis.macle@gmail.com>
2026-04-29 09:41:27 -0500

    Code cleanup

---
+++ test/widgets/utility/process_widgets/session_file_name_desktop_platforms_test.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:journeyers/debug_constants.dart';
+import 'package:journeyers/pages/context_analysis/context_analysis_process.dart';
+import 'package:journeyers/utils/generic/dev/utility_classes_import.dart';
+
+
+void main() 
+{
+    // 'SessionFileNameDesktopPlatforms Tests: \n'
+    group('SessionFileNameDesktopPlatforms Tests: \n', 
+    () 
+    {      
+        // 'The elevated button, for the file picker, is available to the user.'
+        testWidgets('The elevated button, for the file picker, is available to the user.', 
+        (WidgetTester tester) async 
+        {
+          // Setting mock values for SharedPreferences
+          SharedPreferences.setMockInitialValues
+          ({
+              // Setting a value. 
+              // getApplicationFolderPathPref() is called in initState.
+              // Otherwise a "pumpAndSettle timed out" is thrown
+              'applicationFolderPath': ""
+          });
+
+          // Loading the widget
+          await tester.pumpWidget
+          (
+              const MaterialApp(
+              home: Scaffold(
+              body: CAProcess(),
+              ),
+          )
+          );
+          await tester.pumpAndSettle();
+
+          if (testingDebug) pu.printd("Platform: ${Platform.operatingSystem}");
+
+          // Verifying the button text present
+          expect(find.textContaining('Click to save your data'), findsOneWidget);         
+        }
+        );
+    });
+}
