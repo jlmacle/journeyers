407bd98714b9e1724047e4676d509e4435240923
unknown <jeanlouis.macle@gmail.com>
2026-04-30 05:44:20 -0500

    Widget test added

---
+++ test/pages/context_analysis_process_widgets/context_analysis_title_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:journeyers/pages/context_analysis/context_analysis_process_widgets/_context_analysis_form_text_field_misc_constants.dart';
+import 'package:journeyers/pages/context_analysis/context_analysis_process_widgets/1_context_analysis_title_declaration.dart';
+
+void main() {
+  // 'CATitleDeclaration Widget Tests: \n'
+  group('CATitleDeclaration Widget Tests: \n', () 
+  {
+    const String hintText = CAFormTextFieldMiscConstants.caTitleDeclarationHintText;
+
+    // 'Should render the correct text field hint'
+    testWidgets('Should render the correct text field hint', 
+    (WidgetTester tester) async 
+    {
+      // Building the widget
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: CATitleDeclaration
+            (
+              onAnalysisTitleUpdatedCallbackFunction: (_){},
+            )
+          ),
+        ),
+      );
+
+      // Verifying the text field hint present
+      expect(find.text(hintText), findsOneWidget);
+    }
+    );    
+  }
+);  
+
+}
