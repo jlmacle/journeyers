0bbb3e56c0ba75deb64d10952928a26654811091
unknown <jeanlouis.macle@gmail.com>
2026-04-30 02:12:07 -0500

    Code cleanup

--- integration_test/_all_tests.dart
+++ integration_test/_all_tests.dart
@@ -1,3 +1,5 @@
-void main() {
+import 'does_not_pass_as_widget_test_session_file_name_mobile_platforms_test.dart' as does_not_pass_as_widget_test_session_file_name_mobile_platforms_test;
 
+void main() {
+  does_not_pass_as_widget_test_session_file_name_mobile_platforms_test.main();
 }
