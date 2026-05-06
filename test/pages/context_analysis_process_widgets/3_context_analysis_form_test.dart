62744cf684d1a64442fd4b07342182a649669e69
unknown <jeanlouis.macle@gmail.com>
2026-05-06 03:46:01 -0500

    Code cleanup

--- test/pages/context_analysis_process_widgets/3_context_analysis_form_test.dart
+++ test/pages/context_analysis_process_widgets/3_context_analysis_form_test.dart
@@ -24,62 +24,63 @@ void main()
     );
   }
 
+  // Method used to find the expansion tiles
+  Finder getTheExpansionTilesFinder()
+  {
+    return  
+    find.descendant
+    (
+      of: find.byType(ExpansionTile), 
+      matching: find.byType(Text)
+    );
+  }
+  
+
   // 'CAForm Tests: \n'
   group('CAForm Tests: \n', 
   () 
   {  
     // 'Form: Structure: \n'
     group
-  (
-    'Form: Structure: \n',
-    ()
-    {
-      // 'Two perspective expansion tiles are present'
-      testWidgets
-      (
-        'Two perspective expansion tiles are present',
-        (tester) async
-        {
-          await pumpCAForm(tester);
+    (
+      'Form: Structure: \n',
+      ()
+      {
+        // 'Two perspective expansion tiles are present'
+        testWidgets
+        (
+          'Two perspective expansion tiles are present',
+          (tester) async
+          {
+            await pumpCAForm(tester);
 
-          // One tile for the individual perspective, one for the group/team perspective
-          expect(find.byType(ExpansionTile), findsNWidgets(2));
-        },
-      );
+            // One tile for the individual perspective, one for the group/team perspective
+            expect(find.byType(ExpansionTile), findsNWidgets(2));
+          },
+        );
 
-      // 'Individual and group tiles carry the correct heading text'
-      testWidgets
-      (
-        'Individual and group tiles carry the correct heading text',
-        (tester) async
-        {
-          final q = CAQuestionsFields();
-          await pumpCAForm(tester);
+        // 'Individual and group tiles carry the correct heading text'
+        testWidgets
+        (
+          'Individual and group tiles carry the correct heading text',
+          (tester) async
+          {
+            final q = CAQuestionsFields();
+            await pumpCAForm(tester);
 
-          // Verifying that the first expansion tile text is correct
-          var firstExpansionTileTextFinder = find.descendant
-          (
-            of: find.byType(ExpansionTile), 
-            matching: find.byType(Text)
-          ).first;
-          Text firstExpansionTileTextWidget = tester.widget<Text>(firstExpansionTileTextFinder);        
-          expect( firstExpansionTileTextWidget.data, q.level2TitleIndividual);
+            // Verifying that the first expansion tile text is correct
+            var firstExpansionTileTextFinder = getTheExpansionTilesFinder().first;
+            Text firstExpansionTileTextWidget = tester.widget<Text>(firstExpansionTileTextFinder);        
+            expect( firstExpansionTileTextWidget.data, q.level2TitleIndividual);
 
-          // Verifying that the second expansion tile text is correct
-          var secondExpansionTileTextFinder = find.descendant
-          (
-            of: find.byType(ExpansionTile), 
-            matching: find.byType(Text)
-          ).last;
-          Text secondExpansionTileTextWidget = tester.widget<Text>(secondExpansionTileTextFinder);        
-          expect(secondExpansionTileTextWidget.data, q.level2TitleGroup);
-        },
-      );
-      
-    }
-  );
-
-  
+            // Verifying that the second expansion tile text is correct
+            var secondExpansionTileTextFinder = getTheExpansionTilesFinder().last;
+            Text secondExpansionTileTextWidget = tester.widget<Text>(secondExpansionTileTextFinder);        
+            expect(secondExpansionTileTextWidget.data, q.level2TitleGroup);
+          },
+        );        
+      }
+    );
   });
 
 }
