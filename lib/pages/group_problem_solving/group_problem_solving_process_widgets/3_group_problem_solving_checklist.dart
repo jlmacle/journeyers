8cdebc231205faa3a6e64e02dc63fbe73e7d771c
unknown <jeanlouis.macle@gmail.com>
2026-05-09 02:14:57 -0500

    Code cleanup

--- lib/pages/group_problem_solving/group_problem_solving_process_widgets/3_group_problem_solving_checklist.dart
+++ lib/pages/group_problem_solving/group_problem_solving_process_widgets/3_group_problem_solving_checklist.dart
@@ -16,6 +16,7 @@ class _GPSChecklistState extends State<GPSChecklist> {
     "Is our context analysis done?": false,
     "Is the group open to using the app for group problem-solving?": false,
     "Is the group emotionally ready to problem-solve?": false,
+    "Can we get feedback about people’s emotions?": false,
     "Did we agree on what to do if emotions become problematic?": false,
     "Do we agree on the problem that needs to be solved?": false,
     "Did we agree on the order in which to offer the solutions?":false,
