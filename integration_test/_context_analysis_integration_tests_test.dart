d0e0fa6dc51b58b80e982ca106c896a36313c7a8
unknown <jeanlouis.macle@gmail.com>
2026-05-13 08:32:07 -0500

    Integration test added

--- integration_test/_context_analysis_integration_tests_test.dart
+++ integration_test/_context_analysis_integration_tests_test.dart
@@ -444,5 +444,101 @@ void main() {
     },
   ); 
 
+    // 'Session data entered is found on the preview: '
+    // 'not all fields filled: 3: several unchecked checkboxes, several unselected segmented buttons, one empty text field only item \n'
+    // '(assuming an already selected path to the user session data folder)',
+    testWidgets(
+    'Session data entered is found on the preview: '
+    'not all fields filled: 3: several unchecked checkboxes, several unselected segmented buttons, one empty text field only item \n'
+    '(assuming an already selected path to the user session data folder)',
+    (WidgetTester tester) async {
+
+      // Setting mock values for SharedPreferences
+      SharedPreferences.setMockInitialValues
+      ({
+        // Setting value for the first-run modal to be absent,
+        'wasFirstRunModalAcknowledged': true,
+        // and to have the context analysis page, with the dashboard.
+        'wasSessionDataSaved': true,
+        // Temporary test dir as application folder path
+        'applicationFolderPath': testTmpDir!.path
+      });
+
+      // Pumping the CAPage
+      //
+      // pumpWidget renders the first frame.
+      // pumpAndSettle drives the event loop until there are no more pending frames,
+      // letting the async getPreferences() call complete 
+      // and setState(() { _preferencesLoading = false; }) rebuild the tree.
+      //
+      // https://api.flutter.dev/flutter/flutter_test/WidgetTester/pumpAndSettle.html
+      await tester.pumpWidget(buildTestableCAPage());
+      await tester.pumpAndSettle();
+
+      // Verifying that the new process button functions
+      await checkNewCAProcessButtonFunctions(tester);
+
+      // ── TITLE SECTION ─────────────────────────────────────────────────────────────
+      await enterCAProcessTitle(tester, testAnalysisTitle2);
+
+      // ── KEYWORDS SECTION ─────────────────────────────────────────────────────────────
+      await enterCAProcessKeywords(tester, kwsList);
+      
+      // ── FORM SECTION ─────────────────────────────────────────────────────────────
+      // Individual perspective testing values
+      // 7 values are necessary
+      List<bool> checkboxValues = [true, false, false, false, true, true, true];
+      List<String> checkboxTextFieldValues = ["a1", "",  "", "", "a5", "a6", "a7"];       
+      String indivAnotherIssueStrValue = "a8";        
+
+      // Group/teams perspective testing values
+      String groupProblemsToSolveStrValue = "";
+      // 4 values are necessary
+      List<Set<String>> segmentedButtonValues = [{"Yes"},{"No"},{},{"I don't know"}];
+      List<String> segmentedButtonTextFieldValues = ["b2", "b3", "", "b5"];
+      
+      await fillCAForm(tester, checkboxValues, checkboxTextFieldValues, indivAnotherIssueStrValue, 
+      groupProblemsToSolveStrValue, segmentedButtonValues, segmentedButtonTextFieldValues);
+
+      // ── SUBMIT BUTTON SECTION ─────────────────────────────────────────────────────────────
+      Finder fileNameWidgetFinder;
+      if (Platform.isAndroid || Platform.isIOS)
+      {
+        fileNameWidgetFinder =  find.byType(SessionFileNameMobilePlatforms);
+
+        // Path to folder already declared 
+        // Scrolling to make the text field visible for small screens
+        await tester.ensureVisible(fileNameWidgetFinder);
+
+        // Entering a file name
+        await tester.enterText(fileNameWidgetFinder, fileName2WithoutExtension);
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.pumpAndSettle();
+
+        // ── SEARCHING FOR THE METADATA ON THE DASHBOARD SECTION ─────────────────────────────────────────────────────────────
+
+        // Searching for the title
+        expect(find.text(testAnalysisTitle2), findsOne);
+
+        // Searching for the keywords
+        expect(find.text(kw1), findsOne);
+        expect(find.text(kw2), findsOne);
+
+        // ── TESTING THE PREVIEW ─────────────────────────────────────────────────────────────
+        // Putting all non-empty string values together, to retrieve them by index
+        List<String> individualStringValues = 
+          [...checkboxTextFieldValues, indivAnotherIssueStrValue]
+          .where((string) => string.isNotEmpty)
+          .toList();
+  
+        List<String> groupStringValues = [groupProblemsToSolveStrValue, ...segmentedButtonTextFieldValues];
+
+        await testPreview(tester, individualStringValues, segmentedButtonValues, groupStringValues);
+
+        // await tester.pump(const Duration(seconds: 2));
+      }
+    },
+  ); 
+
   });
 }
