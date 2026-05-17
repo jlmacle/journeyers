4ee970c2605485fc7e40eb9f6dfbdf76d0b63702
unknown <jeanlouis.macle@gmail.com>
2026-05-17 08:45:58 -0500

    Code cleanup

--- lib/debug_constants.dart
+++ lib/debug_constants.dart
@@ -12,6 +12,13 @@ const textFieldDebugging = false;
 // Modified in setups
 bool runningTests = false;
 int dateIndex = 0;
-const constDatesList = ["January 01, 2026 10:25 AM",
+const constDatesList = [
+                        "January 30, 2026 10:25 AM",
+                        "January 01, 2026 10:25 AM",
                         "January 15, 2026 10:25 AM",
-                        "January 30, 2026 10:25 AM"];
+                        ];
+const constDatesListSorted = [                        
+                        "January 01, 2026 10:25 AM",
+                        "January 15, 2026 10:25 AM",
+                        "January 30, 2026 10:25 AM",
+                        ];
