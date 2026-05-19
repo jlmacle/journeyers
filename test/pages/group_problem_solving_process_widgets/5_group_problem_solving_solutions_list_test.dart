43010a7f59aebf442e6ad879fe276fa36d328808
unknown <jeanlouis.macle@gmail.com>
2026-05-19 09:06:52 -0500

    Widget test added

---
+++ test/pages/group_problem_solving_process_widgets/5_group_problem_solving_solutions_list_test.dart
@@ -0,0 +1,45 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:journeyers/pages/group_problem_solving/group_problem_solving_process_widgets/5_group_problem_solving_solutions_list.dart';
+import 'package:journeyers/pages/group_problem_solving/group_problem_solving_process_widgets/_group_problem_solving_misc_constants.dart';
+
+void main() 
+{
+  Future<void> pumpGPSSolutionsList(WidgetTester tester) async
+  {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: GPSSolutionsList
+          (
+            solutions: [],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 'GPSSolutionsList Tests: \n'
+  group('GPSSolutionsList Tests: \n', 
+  () 
+  {  
+    // "GPSSolutionsList default aspect: \n"
+    group("GPSSolutionsList default aspect: \n", 
+    () 
+    {
+      // 'The title is present'
+      testWidgets('The title is present', 
+      (WidgetTester tester) async 
+      {
+        // Pumping the widget
+        await pumpGPSSolutionsList(tester);
+
+        // Verifying the title present
+        expect(find.text(solutionsListTitle), findsOne);        
+      });          
+    
+    });
+  });
+}
