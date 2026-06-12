77d12ece1efe5829af3b92a34f08560602759758
unknown <jeanlouis.macle@gmail.com>
2026-06-12 03:04:04 -0500

    Code cleanup

--- lib/widgets/utility/lists/tmp_utility_widgets/list_dashboard_const_strings.dart
+++ lib/widgets/utility/lists/tmp_utility_widgets/list_dashboard_const_strings.dart
@@ -6,7 +6,7 @@ const String listsDashboardTitle = "Stored participants lists";
 // ─── SORTING AND FILTERING ───────────────────────────────────────
 const String filterByKeywordsLabel = "Filter by Keywords";
 const String sortByDateLabel = "Sort by Date";
-const String sortByTitleLabel = "Sort by Title";
+const String sortByTitleLabel = "List sort";
 
 // ─── SESSIONS ITEMS ───────────────────────────────────────
 const String keywordsLabel = "Keywords:";
