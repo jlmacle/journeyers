fc41d0ee2f15065482cef8f6b3aca12d870e3d97
unknown <jeanlouis.macle@gmail.com>
2026-06-16 02:12:15 -0500

    Utils added

--- lib/utils/generic/collections/collections_utils.dart
+++ lib/utils/generic/collections/collections_utils.dart
@@ -10,4 +10,18 @@ class CollectionsUtils
     return set1.difference(set2).isEmpty && set2.difference(set1).isEmpty;
   }
 
+  /// Method used to test that two lists have identical elements when sorted.
+  bool areListsOfEqualSortedContent(List list1, List list2)
+  {
+    if (list1.length != list2.length) {return false;}
+    else 
+    {
+      for (var index = 0; index < list1.length; index++)
+      {
+        if ( (list1..sort())[index] != (list2..sort())[index] ) return false;
+      }
+    }
+    return true;
+  }
+
 }
