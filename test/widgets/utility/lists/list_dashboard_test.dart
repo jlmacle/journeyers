0f9d3d561ba39d4ba5ab4f63fcc8e36fa7fcf56e
unknown <jeanlouis.macle@gmail.com>
2026-06-25 09:29:16 -0500

    Widget test added

---
+++ test/widgets/utility/lists/list_dashboard_test.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+
+import 'package:journeyers/debug_constants.dart';
+import 'package:journeyers/pages/context_analysis/context_analysis_process_widgets/dto_ca_form.dart';
+import 'package:journeyers/widgets/utility/lists/list_dashboard.dart';
+import 'package:journeyers/widgets/utility/lists/list_dashboard_const_strings.dart';
+
+
+// Used to define a folder value for getApplicationSupportPath (PathProvider) 
+class PathProviderPlatformRedirectForTesting extends PathProviderPlatform {
+  PathProviderPlatformRedirectForTesting(this._path);
+
+  final String _path;
+
+  @override
+  Future<String?> getApplicationSupportPath() async => _path;
+}
+
+void main() {
+
+  // ── TESTS PREPARATION AND CLEANUP ─────────────────────────────────────────────────────────────
+  Directory? testTmpDir;
+  
+  setUp(() async {
+    // Creating a temporary folder to store the files to save
+    testTmpDir = await Directory.systemTemp.createTemp('group_problem_solving_integration_test_');
+    PathProviderPlatform.instance = PathProviderPlatformRedirectForTesting(testTmpDir!.path);
+    // To use the alternative saving/reading file paths or to intercept the way the date is saved
+    runningTests = true;
+    dateIndex = 0;
+  });
+
+  // This function will be called after each test is run. The body may be asynchronous; if so, it must return a Future.
+  tearDown(() async {
+    if (testTmpDir!.existsSync()) {
+      // Deleting the temporary folder created to store the saved files
+      await testTmpDir!.delete(recursive: true);
+    }
+  });
+
+  Future<void> pumpTestableWidget(WidgetTester tester)
+  {
+    // Building the widget
+    return tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: ListDashboard
+          (
+            dashboardContext: '', 
+            onAllSessionFilesDeletedContextPageCallbackFunction: () {}, 
+            onParticipantsLoadedCallbackFunction: (_) {}, 
+            onEditSessionDataCallbackFunction: ({required DTOCAForm dtoWhenEdition, required String fileNameWhenEditionWithoutExtension, required bool isSessionDataBeingEdited, required Set<String> keywordsWhenEdition, required String titleWhenEdition}) {}, 
+            dashboardFilteringByKeywordsKey: null,
+          ),
+        ),
+      ),
+    );    
+  }
+
+  group('ListDashboard Tests: \n', () 
+  {
+    // 'Empty dashboard: button toward adding a new list'
+    testWidgets('Empty dashboard: button toward adding a new list', 
+    (WidgetTester tester) async 
+    {
+      // Pumping the widget
+      await pumpTestableWidget(tester);
+      await tester.pumpAndSettle();
+
+      // Verifying the new list button present
+      var newListButtonFinder = find.text(newListButtonLabel);
+      expect(newListButtonFinder, findsOne);
+    });  
+  }); 
+
+}
