aff8f31d53d2db6e5429ecc39e2ab28976b28ac3
unknown <jeanlouis.macle@gmail.com>
2026-07-01 03:40:32 -0500

    Code cleanup

--- lib/pages/context_analysis/context_analysis_page.dart
+++ lib/pages/context_analysis/context_analysis_page.dart
@@ -44,15 +44,15 @@ class CAPageState extends State<CAPage>
   // Values for the keywords at edition time
   Set<String> _keywordsWhenEdition = {};
 
-  // boolean: if an edition is in progress
+  // if an edition is in progress
   bool _isSessionDataBeingEdited = false;
 
   // ─── RUNTIME DATA related data and methods ───────────────────────────────────────
-  // boolean: if runtime data is being loaded
+  // if runtime data is being loaded
   bool _runtimeDataLoading = true;
-  // boolean: if the modal was acknowledged at app first run 
+  // if the modal was acknowledged at app first run 
   bool? _firstRunModalAcknowledged;
-  // boolean: if context analysis data is stored 
+  // if context analysis data is stored 
   bool? _caSessionDataSaved;
 
   // Method used to get stored run-time data
