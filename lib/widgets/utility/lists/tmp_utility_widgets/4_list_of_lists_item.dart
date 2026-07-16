522dab8919c5cc67775515a3d2e84946cc239205
unknown <jeanlouis.macle@gmail.com>
2026-07-16 05:10:17 -0500

    Code cleanup

--- lib/widgets/utility/lists/tmp_utility_widgets/4_list_of_lists_item.dart
+++ lib/widgets/utility/lists/tmp_utility_widgets/4_list_of_lists_item.dart
@@ -195,7 +195,6 @@ class _ListOfListsItemState extends State<ListOfListsItem>
               children: [
                 // Checkbox used for bulk deletion
                 Checkbox(
-                  key: Key("checkbox-${widget.listIndex}"),
                   value: widget.isChecked,
                   onChanged: widget.onCheckboxChangedCallbackFunction,
                 ),
