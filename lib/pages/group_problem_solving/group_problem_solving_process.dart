5dc78362a9e09de83e0562f486216dc26c1cc406
unknown <jeanlouis.macle@gmail.com>
2026-07-16 04:38:16 -0500

    Code cleanup

--- lib/pages/group_problem_solving/group_problem_solving_process.dart
+++ lib/pages/group_problem_solving/group_problem_solving_process.dart
@@ -428,7 +428,6 @@ void _handleCAMetadataSelection(Map<String, dynamic> session) {
                 child: 
                 CustomScrollView
                 (
-                  key: const Key("group-problem-solving-process-scrollview"),
                   slivers: 
                   [
                     const SliverToBoxAdapter
