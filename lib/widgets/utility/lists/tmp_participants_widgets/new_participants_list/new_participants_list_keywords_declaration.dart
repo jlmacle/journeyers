6449ae98905945d52ee48f96efcae75c6ba7ad23
unknown <jeanlouis.macle@gmail.com>
2026-07-17 01:54:43 -0500

    Code cleanup

--- lib/widgets/utility/lists/tmp_participants_widgets/new_participants_list/new_participants_list_keywords_declaration.dart
+++ lib/widgets/utility/lists/tmp_participants_widgets/new_participants_list/new_participants_list_keywords_declaration.dart
@@ -5,6 +5,7 @@ import "package:journeyers/debug_constants.dart";
 import "package:journeyers/pages/group_problem_solving/group_problem_solving_process_widgets/_group_problem_solving_externalized_variables.dart";
 import "package:journeyers/utils/generic/dev/utility_classes_import.dart";
 
+/// {@category Utils - Project-specific}
 /// {@category Lists}
 /// A widget used to declare keywords, or to retrieve keywords from previous context analyses.
 class NewParticipantsKeywordsDeclaration extends StatefulWidget 
