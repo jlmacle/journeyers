7e668a49b24c149d70ad4ae8cf80173173964ec9
unknown <jeanlouis.macle@gmail.com>
2026-07-21 08:31:44 -0500

    Code cleanup

--- lib/utils/generic/dev/type_defs.dart
+++ lib/utils/generic/dev/type_defs.dart
@@ -1,5 +1,5 @@
 /// {@category Utils - Generic}
-/// A function that returns a record,
+/// A typedef for a function that returns a record,
 /// with a boolean (true if a string should be sanitized),
 /// and with a function to sanitize the string.
 typedef StringSanitizerBundle = 
@@ -10,17 +10,17 @@ typedef StringSanitizerBundle =
 Function(String);
 
 /// {@category Utils - Generic}
-/// A function that returns true if a string should be blocked.
+/// A typedef for a function that returns true if a string should be blocked.
 typedef BlacklistingFunction = 
 bool Function(String value);
 
 /// {@category Utils - Generic}
-/// An async function without parameters, and returning Future\<void\>.
+/// A typedef for an async function without parameters, and returning Future\<void\>.
 typedef FutureVoidCallback = 
 Future<void> Function();
 
 /// {@category Utils - Generic}
-/// An async function with a Set\<String\> parameter, and a String parameter.
+/// A typedef for an async function with a Set\<String\> parameter, and a String parameter.
 typedef OnKeywordsUpdatedCallbackFunctionType = 
 Future<void> Function({required String? filePath, required Set<String> updatedKeywords});
 
@@ -48,21 +48,21 @@ Future<void> Function
 });
 
 /// {@category Utils - Project-specific}
-/// An async function typedef with a String parameter, a Set\<String\> parameter, and a Map\<String, dynamic\> parameter,
+/// A typedef for an async function typedef with a String parameter, a Set\<String\> parameter, and a Map\<String, dynamic\> parameter,
 /// and used with the ParticipantsListsItem widget.
 typedef OnParticipantListsItemSetStringUpdatedCallbackFunctionType = 
 Future<void> Function({required String? listKey, required Set<String> updatedItems, required Map<String, dynamic> listData});
 
-/// An async function with a String parameter, and a Map\<String, dynamic\> parameter.
+/// A typedef for an async function with a String parameter, and a Map\<String, dynamic\> parameter.
 typedef OnListDataUpdatedCallbackFunctionType = 
 Future<void> Function({required String? listKey, required Map<String, dynamic> listData});
 
 /// {@category Utils - Generic}
-/// A function with a String parameter, an int parameter, and returning void.
+/// A typedef for a function with a String parameter, an int parameter, and returning void.
 typedef OnListItemValueUpdatedCallbackFunctionType = 
 void Function({required int intParam, required String stringParam});
 
 /// {@category Utils - Generic}
-/// A function with a bool? parameter, and an int parameter.
+/// A typedef for a function with a bool? parameter, and an int parameter.
 typedef OnCheckboxChangedCallbackFunctionType = 
 void Function({required bool? boolParam, required int intParam});
