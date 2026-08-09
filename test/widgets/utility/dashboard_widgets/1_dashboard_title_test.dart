85db3dbcbc4db81216f3350c988b9e86499a5673
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2026-08-09 15:13:24 +0200

    Code cleanup

--- test/widgets/utility/dashboard_widgets/1_dashboard_title_test.dart
+++ test/widgets/utility/dashboard_widgets/1_dashboard_title_test.dart
@@ -1,33 +1,11 @@
-import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
 
-import "package:journeyers/widgets/utility/dashboard/dashboard_widgets/dashboard_const_strings.dart";
-import "package:journeyers/widgets/utility/dashboard/dashboard_widgets/1_dashboard_title.dart";
 
 void main() {
 
   group("DashboardTitle Widget Tests: \n", () 
   {
-    const testTitle = dashboardTitle;
 
-    // "should render the correct title text"
-    testWidgets("Should render the correct title text", 
-    (WidgetTester tester) async 
-    {
-      // Building the widget
-      await tester.pumpWidget(
-        const MaterialApp(
-          home: Scaffold(
-            body: DashboardTitle(title: testTitle),
-          ),
-        ),
-      );
-
-      // Verifying the text present
-      expect(find.text(testTitle), findsOneWidget);
-    }
-    );    
-  }
-);  
+  });  
 
 }
