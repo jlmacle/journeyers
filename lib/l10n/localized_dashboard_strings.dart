0bfa7547868d183c89d1eb2fd121ba896f841da1
unknown <jeanlouis.macle@gmail.com>
2026-08-11 04:28:08 -0500

    Code cleanup

---
+++ lib/l10n/localized_dashboard_strings.dart
@@ -0,0 +1,58 @@
+import "package:flutter/material.dart";
+
+import "package:journeyers/l10n/app_localizations.dart";
+
+class LocalizedDashboardStrings
+{
+  AppLocalizations? _l10n;
+
+  // Sorting and filtering
+  var filterByKeywordsLabel = "";
+  var sortByDate = "";
+  var sortByTitle = "";
+
+  // ─── SESSIONS ITEM ───────────────────────────────────────
+  var keywordsLabel = "";
+
+  // Tooltips
+  var previewTooltipLabel = "";
+  var previewClosingTooltipLabel = "";
+  var editFromDashboardItemTooltipLabel = "";
+  var keywordsTooltipLabel = "";
+  var deleteTooltipLabel = "";
+
+  // Text field labels
+  var keywordsTextFieldLabel = "";
+
+  // Placeholder messages
+  var placeholderForEdit = "";
+
+  // Error messages
+  var emptyTitleEditError = "";
+
+  LocalizedDashboardStrings(BuildContext context)
+  {
+    _l10n = AppLocalizations.of(context);
+    
+    // Sorting and filtering
+    filterByKeywordsLabel = _l10n?.dashboard_filter_by_keywords ?? "Issue with the 'Filter by Keywords' label";
+    sortByDate = _l10n?.dashboard_sort_by_date ?? "Issue with the 'Sort by Date' label";
+    sortByTitle = _l10n?.dashboard_sort_by_title ?? "Issue with the 'Sort by Title' label";
+
+    // ─── SESSIONS ITEM ───────────────────────────────────────
+    keywordsLabel = _l10n?.dashboard_keywords ?? "Issue with the 'Keywords' label";
+    
+    // Tooltips
+    previewTooltipLabel = _l10n?.dashboard_tooltip_preview ?? "Issue with the l10n for the 'Preview' tooltip";
+    previewClosingTooltipLabel = _l10n?.dashboard_preview_close_preview_tooltip ?? "Issue with the l10n for the preview 'Close the preview' tooltip";
+    editFromDashboardItemTooltipLabel = _l10n?.dashboard_tooltip_edit_session_data ?? "Issue with the l10n for the 'Edit session data' tooltip";
+    keywordsTooltipLabel = _l10n?.dashboard_tooltip_edit_keywords ?? "Issue with the l10n for the 'Edit Keywords' tooltip";
+    deleteTooltipLabel = _l10n?.dashboard_tooltip_delete ?? "Issue with the l10n for the 'Delete data' tooltip";
+
+    // Text field labels
+    keywordsTextFieldLabel = _l10n?.dashboard_edit_keywords_sheet_label_text ?? "Issue with the l10n for the 'Keywords Edition' label text";
+
+    // Error messages
+    emptyTitleEditError = _l10n?.dashboard_edit_title_error_empty_title ?? "Issue with the l10n for the 'Title cannot be empty.' error message";
+  }
+}
--- test/widgets/utility/dashboard_widgets/2_dashboard_filtering_feature_test.dart
+++ test/widgets/utility/dashboard_widgets/2_dashboard_filtering_feature_test.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
 
+import "package:journeyers/debug_constants.dart";
 import "package:journeyers/l10n/app_localizations.dart";
 import "package:journeyers/utils/generic/dashboard/dashboard_utils.dart";
-import "package:journeyers/widgets/utility/dashboard/dashboard_widgets/dashboard_const_strings.dart";
+import "package:journeyers/l10n/localized_dashboard_strings.dart";
 import "package:journeyers/widgets/utility/dashboard/dashboard_widgets/2_dashboard_filtering_and_sorting_feature.dart";
 import "package:journeyers/widgets/utility/dashboard/dashboard_widgets/2b_dashboard_sorting_by_date.dart";
 import "package:journeyers/widgets/utility/dashboard/dashboard_widgets/2a_dashboard_sorting_by_title.dart";
@@ -30,6 +31,7 @@ void main() {
       return MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
+        locale: const Locale(testingLocaleOption),
         home: Scaffold(
           body: DashboardSortingAndFilteringFeature(
             dashboardContext: DashboardUtils.caContext,
@@ -44,22 +46,28 @@ void main() {
       );
     }
 
-    testWidgets("Renders all sub-components labels and title text", (WidgetTester tester) async {
+    testWidgets("Renders all sub-components labels and title text", (WidgetTester tester) async 
+    {
       await tester.pumpWidget(createWidgetUnderTest());
+      await tester.pumpAndSettle();
+
+      // Getting the localized strings
+      var context = tester.element(find.byType(Scaffold).first);
+      LocalizedDashboardStrings lds = .new(context);
 
       // Checks for the children and pre-defined labels 
       // sorting by title
       expect(find.byType(DashboardSortingByTitle), findsOneWidget);
       // String interpolation used for the text
-      expect(find.textContaining(sortByTitle), findsOneWidget);
+      expect(find.textContaining(lds.sortByTitle), findsOneWidget);
 
       // sorting by date
       expect(find.byType(DashboardSortingByDate), findsOneWidget);
-      expect(find.text(sortByDate), findsOneWidget);
+      expect(find.text(lds.sortByDate), findsOneWidget);
 
       // filtering by keywords
       expect(find.byType(DashboardFilteringByKeywords), findsOneWidget);
-      expect(find.text(filterByKeywordsLabel), findsOneWidget);     
+      expect(find.text(lds.filterByKeywordsLabel), findsOneWidget);     
     });
 
     testWidgets("Wraps sorting widgets inside a Wrap widget", (WidgetTester tester) async {
