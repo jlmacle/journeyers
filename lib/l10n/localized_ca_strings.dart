032dccc2c06debb98e5288cb876723d8d511eba6
unknown <jeanlouis.macle@gmail.com>
2026-08-12 01:30:15 -0500

    Code cleanup

--- lib/l10n/localized_ca_strings.dart
+++ lib/l10n/localized_ca_strings.dart
@@ -6,13 +6,26 @@ class LocalizedCAStrings
 {
   AppLocalizations? _l10n;
 
+  // CA dashboard title
   var dashboardTitle = "";
+
+  // Segmented button options
+  var yes = "";
+  var no = "";
+  var iDontKnow = "";
+          
 
   LocalizedCAStrings(BuildContext context)
   {
     _l10n = AppLocalizations.of(context);
     
     dashboardTitle = _l10n?.ca_dashboard_title ?? "Issue with the dashboard title";
+  
+    yes =  _l10n?.segmented_button_yes ?? "Issue with the l10n for Yes";
+    no = _l10n?.segmented_button_no ?? "Issue with the l10n for No";
+    iDontKnow = _l10n?.segmented_button_I_don_t_know ?? "Issue with the l10n for I don't know";
+          
+  
   }
 
 }
