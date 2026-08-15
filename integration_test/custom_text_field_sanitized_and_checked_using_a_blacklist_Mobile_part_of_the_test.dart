81c462a5d6fc590be506d8423933f990ed48b960
unknown <jeanlouis.macle@gmail.com>
2026-08-15 08:15:06 -0500

    Code cleanup

--- integration_test/custom_text_field_sanitized_and_checked_using_a_blacklist_Mobile_part_of_the_test.dart
+++ integration_test/custom_text_field_sanitized_and_checked_using_a_blacklist_Mobile_part_of_the_test.dart
@@ -33,76 +33,7 @@ void main() {
   
   group("TextFieldChecked Tests (Mobile platforms):\n", () {       
 
-    // Couldn"t test with a POSIX path.
-    // Kept to illustrate that the idea wasn"t overlooked.
-    // "On mobile: The user cannot submit a CSV file name already used."
-    testWidgets("On mobile: The user cannot submit a CSV file name already used.", 
-    (WidgetTester tester) async 
-    {
-      
-      if (testingDebug) pu.printd("Testing Debug: testTmpDir path: ${testTmpDir!.path}");
-      // Setting mock values for SharedPreferences
-      SharedPreferences.setMockInitialValues
-      ({
-          // To avoid the first-run modal
-          "wasFirstRunModalAcknowledged": true, 
-          // To get the CA process page
-          "wasCASessionDataSaved": false,
-          // Test dir as application folder path
-          "applicationFolderPath": testTmpDir!.path
-      });
-
-      // Loading the widget
-      await tester.pumpWidget
-      (
-        const MaterialApp(
-          home: Scaffold(
-            // du.getStoredFileNamesOnMobile(); is run in main.dart
-            body: GPSapp()
-          ),
-        ),
-      );
-      await tester.pumpAndSettle();
-
-      if (testingDebug) pu.printd("Testing Debug: Platform: ${Platform.operatingSystem}");
-      if (Platform.isAndroid || Platform.isIOS)
-      {
-        // Writing CSV files in the tmp folder 
-        File csvFile1 = File("${pathToTmpFolder!}/file1.csv");
-        await csvFile1.create();
-
-        File csvFile2 = File("${pathToTmpFolder!}/file2.csv");
-        await csvFile2.create();
-
-        List<File> fileList = await 
-        fu.getFilesWithExtensionInDirectory
-        (directoryPath: pathToTmpFolder!, fileExtension: "csv", searchIsRecursive: true);
-
-        if (testingDebug) pu.printd("Testing Debug: Files found in ${pathToTmpFolder!}:\n $fileList");
-
-        // Was calling du.getStoredFileNamesOnMobile(pathToTmpFolder!),
-        // with a modified version of getStoredFileNamesOnMobile() and of the Kotlin code.
-        // The path was an invalid URI for SAF, therefore throwing an exception.
-        // The following PlatformException was thrown running a test:
-        // PlatformException(INVALID_URI, Expected a SAF content:// tree URI, got:
-        // /data/user/0/dev.journeyers/code_cache/SessionFileNameOnMobilePlatforms_testKCXGHA, null, null)
-
-        // Making sure the text field visible
-        var textFieldFinder = 
-        find.descendant(
-          of: find.byType(SessionFileNameOnMobilePlatforms), 
-          matching: find.byType(TextField));
-        await tester.ensureVisible(textFieldFinder);
-
-        // Entering the text to search in the blacklist          
-        await tester.enterText(textFieldFinder, csvFileNameBlacklisted);       
-        await tester.pumpAndSettle();
-
-        // Verifying error message is rendered
-        // expect(find.text(TextFieldUtils.errorFileNameAlreadyUsed), findsOneWidget);
-      }                  
-    }
-    );
+    
 
     // "On mobile: The user cannot submit a TXT file name already used."
     testWidgets("On mobile: The user cannot submit a TXT file name already used.", 
