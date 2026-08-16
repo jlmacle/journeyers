d222fff5ee9554064082cfb0bc7a32d65d9ef362
unknown <jeanlouis.macle@gmail.com>
2026-08-16 09:25:50 -0500

    Code cleanup

--- lib/widgets/utility/process/process_const_strings.dart
+++
@@ -1,5 +0,0 @@
-/// The text of the folder picker for Android.
-const labelFolderPickerAndroid = "Please select or create a folder\nfor app storage.";
-
-/// The text of the folder picker for IOS.
-const labelFolderPickerIOS = "Please select a folder\nfor app storage.";
