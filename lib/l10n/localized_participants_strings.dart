edef2936c776c56349bd711d66baff47614eaa7d
unknown <jeanlouis.macle@gmail.com>
2026-08-17 03:06:50 -0500

    Code cleanup

--- lib/l10n/localized_participants_strings.dart
+++ lib/l10n/localized_participants_strings.dart
@@ -6,13 +6,19 @@ class LocalizedParticipantsStrings
 {
   AppLocalizations? _l10n;
 
+  // The title for the participants dashboard
   var listsDashboardTitle = "";
+  // Sorting by list name label
+  var listsSortByLabel = "" ;
 
   LocalizedParticipantsStrings(BuildContext context)
   {
     _l10n = AppLocalizations.of(context);    
     
+     // The title for the participants dashboard
     listsDashboardTitle = _l10n?.text_lists_dashboard_title ?? "Issue with the title for the participants lists dashboard";
-   
+    // Sorting by list name label
+    listsSortByLabel = _l10n?.text_lists_dashboard_sort_by_list_name ?? "Issue with the l10n for the 'List sort' label";
+ 
   }
 }
