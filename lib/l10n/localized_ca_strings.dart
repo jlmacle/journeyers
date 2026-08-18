e885b5257d3440b5167628b8ace499fbe22f7f23
unknown <jeanlouis.macle@gmail.com>
2026-08-18 07:31:43 -0500

    Code cleanup

--- lib/l10n/localized_ca_strings.dart
+++ lib/l10n/localized_ca_strings.dart
@@ -6,28 +6,45 @@ class LocalizedCAStrings
 {
   AppLocalizations? _l10n;
 
-  // CA dashboard title
-  var dashboardTitle = "";
+  // ─── CA PROCESS ───────────────────────────────────────
+  // ─── Title ───────────────────────────────────────
+  var caTitleTextFieldHint = "";
 
+  // ─── CA Form ───────────────────────────────────────
   var invitationToUnfoldExpansionTile = "";
 
   // Segmented button options
   var yes = "";
   var no = "";
   var iDontKnow = "";
+
+  // ─── DASHBOARD ───────────────────────────────────────
+  // CA dashboard title
+  var dashboardTitle = "";
+
+
           
 
   LocalizedCAStrings(BuildContext context)
   {
     _l10n = AppLocalizations.of(context);
     
-    dashboardTitle = _l10n?.ca_dashboard_title ?? "Issue with the dashboard title";
+    // ─── CA PROCESS ───────────────────────────────────────
+    // ─── Title ───────────────────────────────────────
+    caTitleTextFieldHint = _l10n?.ca_process_title_text_field_hint ?? "Issue with the context analysis title text field hint";
 
+    // ─── CA Form ───────────────────────────────────────
     invitationToUnfoldExpansionTile = _l10n?.ca_process_invitation_to_unfold_expansion_tile ?? "Issue with the l10n for 'Please click to unfold'";
   
     yes =  _l10n?.segmented_button_yes ?? "Issue with the l10n for Yes";
     no = _l10n?.segmented_button_no ?? "Issue with the l10n for No";
     iDontKnow = _l10n?.segmented_button_I_don_t_know ?? "Issue with the l10n for I don't know";
+    
+    // ─── DASHBOARD ───────────────────────────────────────
+    // CA dashboard title
+    dashboardTitle = _l10n?.ca_dashboard_title ?? "Issue with the dashboard title";
+
+    
           
   
   }
