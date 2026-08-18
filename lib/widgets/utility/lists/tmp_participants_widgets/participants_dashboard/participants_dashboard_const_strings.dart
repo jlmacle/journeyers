5c11e4d2bf1255ef91c0334274268b244e0592b8
unknown <jeanlouis.macle@gmail.com>
2026-08-18 04:18:37 -0500

    Code cleanup

--- lib/widgets/utility/lists/tmp_participants_widgets/participants_dashboard/participants_dashboard_const_strings.dart
+++ lib/widgets/utility/lists/tmp_participants_widgets/participants_dashboard/participants_dashboard_const_strings.dart
@@ -4,11 +4,5 @@
 // Buttons
 const newListButtonLabel = "Please tap to add a list.";
 
-// Text field labels
-const keywordsTextFieldLabel = "Keywords Edition (please separate with commas)";
-const participantsTextFieldLabel = "Participants Edition (please separate with commas)";
-const listNameTextFieldLabel = "List Name Edition";
-
 // Error messages
-const emptyParticipantsListError = "Participants list cannot be empty.";
 const emptyLabelEditError = "Label cannot be empty.";
