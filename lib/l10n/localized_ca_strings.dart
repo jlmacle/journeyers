634b32e7ef1029b3d090d07627e89d58e4a94c01
unknown <jeanlouis.macle@gmail.com>
2026-08-19 09:35:59 -0500

    Code cleanup

--- lib/l10n/localized_ca_strings.dart
+++ lib/l10n/localized_ca_strings.dart
@@ -39,7 +39,7 @@ class LocalizedCAStrings
     caTitleTextFieldHint = _l10n?.ca_process_title_text_field_hint ?? "Issue with the context analysis title text field hint";
 
     // ─── Keywords ───────────────────────────────────────
-    caKeywordsTextFieldHint = _l10n?.ca_process_keywords_text_field_hint ?? "Issue with the text field hint, for the keywords of the context analysis process";
+    caKeywordsTextFieldHint = _l10n?.l10n_keywords_entry_text_field_hint ?? "Issue with the l10n for the keywords entry text field hint";
 
     // ─── CA Form ───────────────────────────────────────
     invitationToUnfoldExpansionTile = _l10n?.ca_process_invitation_to_unfold_expansion_tile ?? "Issue with the l10n for 'Please click to unfold'";
