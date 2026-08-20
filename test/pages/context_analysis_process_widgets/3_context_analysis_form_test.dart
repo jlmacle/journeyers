00666b6c8e23f6d8c05322951401fafc2ce10d74
unknown <jeanlouis.macle@gmail.com>
2026-08-20 02:12:05 -0500

    Code cleanup

--- test/pages/context_analysis_process_widgets/3_context_analysis_form_test.dart
+++ test/pages/context_analysis_process_widgets/3_context_analysis_form_test.dart
@@ -150,7 +150,7 @@ void main()
             // Verifying consistency between hard-coded string and localized string
             expect(level2TitleGroup, lqf.level2TitleGroup);     
 
-            // Verifying the first title correct
+            // Verifying the second title correct
             expect(secondExpansionTileTextWidget.data, lqf.level2TitleGroup);
           },
         ); 
