852359c61f1b7034a7aa42852d950c833198af20
unknown <jeanlouis.macle@gmail.com>
2026-08-20 04:12:40 -0500

    Code cleanup

--- lib/l10n/localized_ca_strings.dart
+++ lib/l10n/localized_ca_strings.dart
@@ -16,6 +16,10 @@ class LocalizedCAStrings
 
   // ─── CA Form ───────────────────────────────────────
   var invitationToUnfoldExpansionTile = "";
+
+  // Text field hints
+  var pastOutcomesTextFieldHint = "";
+  var helpingAndHouseholdTextFieldHint = "";
   var caFormPleaseDevelopTextFieldHint = "";
 
   // Segmented button options
@@ -44,6 +48,9 @@ class LocalizedCAStrings
     // ─── CA Form ───────────────────────────────────────
     invitationToUnfoldExpansionTile = _l10n?.ca_process_invitation_to_unfold_expansion_tile ?? "Issue with the l10n for 'Please click to unfold'";
   
+    // Text field hints
+    pastOutcomesTextFieldHint = _l10n?.ca_process_past_outcomes_text_field_hint ?? "Issue with the text field hint, for the past outcomes of the problem for the household";
+    helpingAndHouseholdTextFieldHint = _l10n?.ca_process_helping_household_text_field_hint ?? "Issue with the text field hint, for the reasons and potential impacts of an imbalance between faithfulness towards your own and consideration towards others";
     caFormPleaseDevelopTextFieldHint = _l10n?.ca_process_please_develop_text_field_hint ?? "Issue with the text field hint inviting to develop";
 
     yes =  _l10n?.segmented_button_yes ?? "Issue with the l10n for Yes";
