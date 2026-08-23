9be93aca3d01087d7c24571f656c1c10e7f0e44c
unknown <jeanlouis.macle@gmail.com>
2026-08-23 03:23:14 -0500

    Code cleanup

--- test/pages/group_problem_solving_process_widgets/3_group_problem_solving_checklist_test.dart
+++ test/pages/group_problem_solving_process_widgets/3_group_problem_solving_checklist_test.dart
@@ -1,12 +1,16 @@
 // ignore: file_names
 import "package:flutter/material.dart";
 import "package:flutter_test/flutter_test.dart";
+import "package:journeyers/debug_constants.dart";
+import "package:journeyers/l10n/app_localizations.dart";
 
 import "package:journeyers/l10n/localized_gps_strings.dart";
 import "package:journeyers/pages/group_problem_solving/group_problem_solving_process_widgets/3_group_problem_solving_checklist.dart";
 import "package:journeyers/pages/group_problem_solving/group_problem_solving_process_widgets/_group_problem_solving_externalized_variables.dart";
+import "package:journeyers/utils/generic/dev/utility_classes_import.dart";
 
 import "../../../integration_test/externalized_code/externalized_testing_code.dart";
+import "../../_widget_testing_utils/widget_testing_utils.dart";
 
 void main() 
 {
@@ -14,6 +18,9 @@ void main()
   {
     await tester.pumpWidget(
       const MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        locale: Locale(testingLocaleOption),
         home: Scaffold(
           body: GPSChecklist(),
         ),
@@ -27,7 +34,6 @@ void main()
     group("GPSChecklist default aspect: \n", 
     () 
     { 
-      // "The default rectangle color is orange"
       testWidgets("The default rectangle color is orange", 
       (WidgetTester tester) async 
       {
@@ -35,12 +41,11 @@ void main()
         await pumpGPSChecklist(tester);
 
         // Verifying the default rectangle color is orange
-        await gpsTestChecklistTitleBorderColor(tester, rectangleColor);        
+        await gpsTestChecklistTitleBorderColor(tester, rectangleColor);
       });          
-      });
+      
 
-      // "The title is present"
-      testWidgets("The title is present", 
+      testWidgets("The correct title is present", 
       (WidgetTester tester) async 
       {
         // Pumping the widget
@@ -50,9 +55,26 @@ void main()
         var context = tester.element(find.byType(Scaffold).first);
         LocalizedGPSStrings lgps = .new(context);
 
+        // Default title hard-coded strings
+        var defaultTitle = "";
+        
+        var localeLanguageCode = getLocaleLanguageCode(tester);
+
+        switch(localeLanguageCode.toLowerCase())
+        {
+          case("en"): { defaultTitle = "Checklist"; }
+          case("fr"): { defaultTitle = "Check-list"; }        
+        }
+        if (testingDebug) pu.printd("Testing Debug: defaultTitle: $defaultTitle");
+
+        // Verifying consistency between hard-coded string and localized string
+        expect(defaultTitle, lgps.checkListTitle);
+
         // Verifying the title present
         expect(find.text(lgps.checkListTitle), findsOne);        
-      });          
+      });  
+
+    });        
     
     });
   
