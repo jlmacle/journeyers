dccf2b87633271a2d948f0250a06d65dbb5f060b
unknown <jeanlouis.macle@gmail.com>
2026-08-23 04:36:02 -0500

    Code cleanup

--- lib/l10n/localized_gps_strings.dart
+++ lib/l10n/localized_gps_strings.dart
@@ -22,6 +22,7 @@ class LocalizedGPSStrings
   // GPSChecklist
   var checkListTitle = "";
   var checkListAppBarTitle = "";
+  var checkListQuestion1 = "";
 
   // GPSKeywordsDeclaration
   var gpsKeywordsTitle = "";
@@ -57,7 +58,8 @@ class LocalizedGPSStrings
     // GPSChecklist
     checkListTitle = _l10n?.gps_process_checklist_title ?? "Issue with the title for the checklist-related widget.";
     checkListAppBarTitle = _l10n?.gps_process_checklist_invitation ?? "Issue with the invitation before the checklist";
-    
+    checkListQuestion1 = _l10n?.gps_process_checklist_question_1 ?? "Issue with the l10n for the checklist question 1";
+
     // GPSKeywordsDeclaration
     gpsKeywordsTitle = _l10n?.l10n_keywords ?? "Issue with the l10n for 'Keywords'.";
     gpsKeywordsDeclarationOverlayCloseIconButtonToolTip = _l10n?.l10n_keywords_overlay_close_button_tooltip ?? "Issue with the l10n for the keywords overlay close button tooltip";                 
