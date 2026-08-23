f870adba0c3c31c3bd2e7184e1ede01e6cb42afd
unknown <jeanlouis.macle@gmail.com>
2026-08-23 08:40:53 -0500

    Widget test added

--- test/pages/group_problem_solving_process_widgets/6_group_problem_solving_new_idea_test.dart
+++ test/pages/group_problem_solving_process_widgets/6_group_problem_solving_new_idea_test.dart
@@ -6,6 +6,9 @@ import "package:journeyers/debug_constants.dart";
 import "package:journeyers/l10n/app_localizations.dart";
 import "package:journeyers/l10n/localized_gps_strings.dart";
 import "package:journeyers/pages/group_problem_solving/group_problem_solving_process_widgets/6_group_problem_solving_new_idea.dart";
+import "package:journeyers/utils/generic/dev/utility_classes_import.dart";
+
+import "../../_widget_testing_utils/widget_testing_utils.dart";
 
 void main() 
 {
@@ -32,8 +35,7 @@ void main()
     group("GPSNewIdea default aspect: \n", 
     () 
     {      
-      // "The hint text is present"
-      testWidgets("The hint text is present", 
+      testWidgets("The correct hint text is present", 
       (WidgetTester tester) async 
       {
         // Pumping the widget
@@ -43,8 +45,23 @@ void main()
         var context = tester.element(find.byType(Scaffold).first);
         LocalizedGPSStrings lgps = .new(context);
 
-        // Verifying the hint text present
-        expect(find.text(lgps.newIdeaTextFieldHint), findsOne);        
+        // newIdeaTextFieldHint hard-coded strings
+        var newIdeaTextFieldHint = "";
+        
+        var localeLanguageCode = getLocaleLanguageCode(tester);
+
+        switch(localeLanguageCode.toLowerCase())
+        {
+          case("en"): { newIdeaTextFieldHint = "Please enter an idea."; }
+          case("fr"): { newIdeaTextFieldHint = "Veuillez entrer une idée."; }        
+        }
+        if (testingDebug) pu.printd("Testing Debug: newIdeaTextFieldHint: $newIdeaTextFieldHint");
+
+        // Verifying consistency between hard-coded string and localized string
+        expect(newIdeaTextFieldHint, lgps.newIdeaTextFieldHint);     
+
+        // Verifying the newIdeaTextFieldHint present
+        expect(find.text(lgps.newIdeaTextFieldHint), findsOne);          
       });          
     
     });
