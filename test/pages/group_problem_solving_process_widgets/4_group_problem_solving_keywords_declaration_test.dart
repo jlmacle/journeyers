039562ced7ca0f990f93383d9426788b442c425e
unknown <jeanlouis.macle@gmail.com>
2026-08-23 02:15:58 -0500

    Widget test added

--- test/pages/group_problem_solving_process_widgets/4_group_problem_solving_keywords_declaration_test.dart
+++ test/pages/group_problem_solving_process_widgets/4_group_problem_solving_keywords_declaration_test.dart
@@ -4,6 +4,7 @@ import "package:flutter_test/flutter_test.dart";
 
 import "package:journeyers/debug_constants.dart";
 import "package:journeyers/l10n/app_localizations.dart";
+import "package:journeyers/l10n/localized_dashboard_strings.dart";
 import "package:journeyers/l10n/localized_gps_strings.dart";
 import "package:journeyers/pages/group_problem_solving/group_problem_solving_process_widgets/4_group_problem_solving_keywords_declaration.dart";
 import "package:journeyers/utils/generic/dev/utility_classes_import.dart";
@@ -18,6 +19,7 @@ void main()
       MaterialApp(
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
+        locale: const Locale(testingLocaleOption),
         home: Scaffold(
           body: GPSKeywordsDeclaration
           (
@@ -104,6 +106,43 @@ void main()
 
         // Verifying the appbar title present
         expect(find.text(lgps.gpsKeywordsOverlayAppbarTitle), findsOne);        
+      });          
+    
+
+      testWidgets("The correct text hint is present", 
+      (WidgetTester tester) async 
+      {
+        // Pumping the widget
+        await pumpGPSKeywordsDeclaration(tester);
+        await tester.pumpAndSettle();
+
+        // Getting the localized strings
+        var context = tester.element(find.byType(Scaffold).first);
+        LocalizedGPSStrings lgps = .new(context);
+        LocalizedDashboardStrings lds = .new(context);        
+
+        // Tapping to open the overlay
+        var keywordsTitleFinder = find.text(lgps.gpsKeywordsTitle);
+        await tester.tap(keywordsTitleFinder);
+        await tester.pumpAndSettle();        
+
+        // Hint hard-coded strings
+        var hintText = "";
+        
+        var localeLanguageCode = getLocaleLanguageCode(tester);
+     
+        switch(localeLanguageCode.toLowerCase())
+        {
+          case("en"): { hintText = "Please enter one keyword at a time\n(+ Enter key)."; }
+          case("fr"): { hintText = "Veuillez renseigner un mot-clé à la fois\n(+ Touche Entrée)."; }        
+        }
+        if (testingDebug) pu.printd("Testing Debug: hintText: $hintText"); 
+
+        // Verifying consistency between hard-coded string and localized string
+        expect(hintText, lds.keywordsEntryTextFieldHint);     
+
+        // Verifying the text field hint present
+        expect(find.text(lds.keywordsEntryTextFieldHint), findsOneWidget);     
       });          
     
     });
