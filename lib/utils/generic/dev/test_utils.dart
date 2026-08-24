761834759b9a4da38c3df5d823d2474fc34a3ceb
unknown <jeanlouis.macle@gmail.com>
2026-08-24 03:48:26 -0500

    Code cleanup

--- lib/utils/generic/dev/test_utils.dart
+++ lib/utils/generic/dev/test_utils.dart
@@ -1,4 +1,8 @@
 import "package:flutter/material.dart";
+import "package:flutter_test/flutter_test.dart";
+
+import "package:journeyers/debug_constants.dart";
+import "package:journeyers/utils/generic/dev/utility_classes_import.dart";
 
 import "package:path_provider_platform_interface/path_provider_platform_interface.dart";
 
@@ -17,3 +21,33 @@ class PathProviderPlatformRedirectForTesting extends PathProviderPlatform {
 /// A getter to test if a test is being run.
 bool get isInTestEnvironment =>
     WidgetsBinding.instance.runtimeType.toString().contains("Test");
+
+/// {@category Utils - Generic}
+/// A method used to print all Text widgets data.
+void printTextData(WidgetTester tester)
+{
+    var textsFinder = find.byType(Text);
+    for (var i= 0 ; i < textsFinder.evaluate().length; i++)
+    {
+      var textFinder = textsFinder.at(i);
+      var textWidget = tester.widget<Text>(textFinder);
+      if (testingDebug) pu.printd("Testing Debug: textWidget.data: ${textWidget.data}");
+    }
+}
+
+/// {@category Utils - Generic}
+/// A method used to test if a CircularProgressIndicator is present.
+void isCircularProgressIndicatorPresent(WidgetTester tester)
+{
+  var circularProgressIndicatorFinder = find.byType(CircularProgressIndicator);
+  bool isPresent = ( circularProgressIndicatorFinder.evaluate().isNotEmpty );
+  if(isPresent)
+  {
+    if (testingDebug) pu.printd("Testing Debug: CircularProgressIndicator present");
+  }
+  else
+  {
+    if (testingDebug) pu.printd("Testing Debug: CircularProgressIndicator absent");
+  }
+
+}
