a754588a7df34bb0581532fa0c1dca26debf2861
unknown <jeanlouis.macle@gmail.com>
2026-08-25 04:52:46 -0500

    Code cleanup

--- lib/l10n/localized_gps_strings.dart
+++ lib/l10n/localized_gps_strings.dart
@@ -26,6 +26,7 @@ class LocalizedGPSStrings
   var checkListQuestion2 = "";
   var checkListQuestion3 = "";
   var checkListQuestion4 = "";
+  var checkListQuestion5 = "";
 
   var closeChecklistTooltipLabel = "";
 
@@ -69,6 +70,8 @@ class LocalizedGPSStrings
     checkListQuestion2 = _l10n?.gps_process_checklist_question_2 ?? "Issue with the l10n for the checklist question 2";
     checkListQuestion3 = _l10n?.gps_process_checklist_question_3 ?? "Issue with the l10n for the checklist question 3";
     checkListQuestion4 = _l10n?.gps_process_checklist_question_4 ?? "Issue with the l10n for the checklist question 4";
+    checkListQuestion5 = _l10n?.gps_process_checklist_question_5 ?? "Issue with the l10n for the checklist question 5";
+
 
     closeChecklistTooltipLabel = _l10n?.gps_process_checklist_close_overlay_tooltip ?? "Issue with the l10n for the 'Close checklist' tooltip";
     
