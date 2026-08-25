9275989c222577c045178f176ed0faac3899ce08
unknown <jeanlouis.macle@gmail.com>
2026-08-25 02:38:38 -0500

    Code cleanup

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -496,7 +496,7 @@ abstract class AppLocalizations {
   /// l10n for the checklist question 4
   ///
   /// In en, this message translates to:
-  /// **'Is the group emotionally ready to problem-solve?'**
+  /// **'Is the group emotionally capable to problem-solve?'**
   String get gps_process_checklist_question_4;
 
   /// l10n for the 'Close checklist' tooltip
