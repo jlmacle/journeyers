67659acf9955391bdeafe317eb2b85082b795ff7
unknown <jeanlouis.macle@gmail.com>
2026-08-30 03:03:29 -0500

    Code cleanup

--- integration_test/session_file_name_mobile_platforms_As_a_widget_test_runs_on_Windows_even_if_Android_is_specified_as_device_test.dart
+++ integration_test/session_file_name_mobile_platforms_As_a_widget_test_runs_on_Windows_even_if_Android_is_specified_as_device_test.dart
@@ -18,8 +18,6 @@ void main()
     group("SessionFileNameOnMobilePlatforms Tests: \n", 
     () 
     {      
-        // "On mobile: The folder picker is available to the user, when the user didn't select the folder for its files yet"
-        // Important: As a widget test, runs on Windows, even if Android is selected as a device.
         testWidgets("On mobile: The folder picker is available to the user, when the user didn't select of folder for its files yet.", 
         (WidgetTester tester) async 
         {
@@ -58,8 +56,6 @@ void main()
         }
         );
 
-        // "On mobile: The text field is available for the user to enter the file name, when the user did select a folder for its files"
-        // Important: As a widget test, runs on Windows, even if Android is selected as a device.
         testWidgets("On mobile: The text field is available for the user to enter the file name, when the user did select a folder for its files.", 
         (WidgetTester tester) async 
         {
