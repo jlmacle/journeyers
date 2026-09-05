3c89970ec7d67b76e31e37c8636cc1ba7f7042c4
unknown <jeanlouis.macle@gmail.com>
2026-09-05 03:52:03 -0500

    Code cleanup

--- test/widgets/utility/process_widgets/session_file_name_mobile_platforms_As_a_widget_test_runs_on_Windows_even_if_Android_is_specified_as_device_test.dart
+++ test/widgets/utility/process_widgets/session_file_name_mobile_platforms_As_a_widget_test_runs_on_Windows_even_if_Android_is_specified_as_device_test.dart
@@ -9,14 +9,10 @@ import "package:journeyers/debug_constants.dart";
 import "package:journeyers/pages/context_analysis/context_analysis_process.dart";
 import "package:journeyers/utils/generic/dev/utility_classes_import.dart";
 
-void main() 
-{
+void main() {
   const textFieldHintPart = "Please add the";
 
-    group("SessionFileNameOnMobilePlatforms Tests: \n", 
-    () 
-    {      
-        // "On mobile: The folder picker is available to the user, when the user didn't select the folder for its files yet"
+    group("SessionFileNameOnMobilePlatforms Tests: \n", () {      
         // Important: As a widget test, runs on Windows, even if Android is selected as a device.
         testWidgets("On mobile: The folder picker is available to the user, when the user didn't select of folder for its files yet.", 
         (WidgetTester tester) async 
@@ -57,7 +53,6 @@ void main()
         }
         );
 
-        // "On mobile: The text field is available for the user to enter the file name, when the user did select a folder for its files"
         // Important: As a widget test, runs on Windows, even if Android is selected as a device.
         testWidgets("On mobile: The text field is available for the user to enter the file name, when the user did select a folder for its files.", 
         (WidgetTester tester) async 
